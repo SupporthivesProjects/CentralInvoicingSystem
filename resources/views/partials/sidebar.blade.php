99f2faea188b303bcced7f4da26b884a557381b2
Narayan Zade <gadgetzne@gadgetznes-MacBook-Air.local>
2025-04-08 12:03:58 +0530

    side menus list  updated

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,14 +2,10 @@
 <aside class="app-sidebar sticky" id="sidebar">
 
     <!-- Start::main-sidebar-header -->
+    
     <div class="main-sidebar-header">
-        <a href="index.html" class="header-logo">
-            <img src="{{ asset('images/brand-logos/desktop-white.png') }}" class="desktop-white" alt="logo">
-            <img src="{{ asset('images/brand-logos/toggle-white.png') }}" class="toggle-white" alt="logo">
-            <img src="{{ asset('images/brand-logos/desktop-logo.png') }}" class="desktop-logo" alt="logo">
-            <img src="{{ asset('images/brand-logos/toggle-dark.png') }}" class="toggle-dark" alt="logo">
-            <img src="{{ asset('images/brand-logos/toggle-logo.png') }}" class="toggle-logo" alt="logo">
-            <img src="{{ asset('images/brand-logos/desktop-dark.png') }}" class="desktop-dark" alt="logo">
+        <a href="{{ route('dashboard') }}" class="header-logo text-white text-2xl font-bold">
+            <span class="text-logo">Central Invoice System</span>
         </a>
     </div>
     <!-- End::main-sidebar-header -->
@@ -37,11 +33,48 @@
                     </a>
                 </li>
                 <!-- End::slide -->
-                <!-- Start::slide__category -->
-                <li class="slide__category"><span class="category-name">Business Models</span></li>
+
+                 <!-- Start::slide__category -->
+                <li class="slide__category"><span class="category-name">Connected Sites</span></li>
                 <!-- End::slide__category -->
 
                 <!-- Start::slide -->
+                <!-- ECommerce -->
+               <!-- Start::Sites Menu -->
+                <li class="slide has-sub">
+                    <a href="javascript:void(0);" class="side-menu__item">
+                        <span class="shape1"></span>
+                        <span class="shape2"></span>
+                        <i class="ti-world side-menu__icon"></i> <!-- Updated icon -->
+                        <span class="side-menu__label">Manage Sites</span>
+                        <i class="fe fe-chevron-right side-menu__angle"></i>
+                    </a>
+                    <ul class="slide-menu child1">
+                        <li class="slide side-menu__label1">
+                            <a href="javascript:void(0)">Manage Sites</a>
+                        </li>
+                        <li class="slide">
+                            <a href="ecommerce-create-invoice.html" class="side-menu__item">
+                                Connect New Site 
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="ecommerce-invoice-history.html" class="side-menu__item d-flex align-items-center justify-content-between">
+                                <div>
+                                    <span>All Connected Sites</span>
+                                </div>
+                                <span class="badge bg-success">546</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- End::Sites Menu -->
+
+                <!-- Start::slide__category -->
+                <li class="slide__category"><span class="category-name">Business Models</span></li>
+                <!-- End::slide__category -->
+                 <!-- Start::slide -->
+
                 <!-- ECommerce -->
                 <li class="slide has-sub">
                     <a href="javascript:void(0);" class="side-menu__item">
@@ -54,7 +87,14 @@
                     <ul class="slide-menu child1">
                         <li class="slide side-menu__label1"><a href="javascript:void(0)">ECommerce</a></li>
                         <li class="slide"><a href="ecommerce-create-invoice.html" class="side-menu__item">Generate Invoice</a></li>
-                        <li class="slide"><a href="ecommerce-invoice-history.html" class="side-menu__item">View Invoice Records</a></li>
+                        <li class="slide">
+                            <a href="ecommerce-invoice-history.html" class="side-menu__item d-flex align-items-center justify-content-between">
+                                <div>
+                                    <span>Connected Sites</span>
+                                </div>
+                                <span class="badge bg-success">34</span>
+                            </a>
+                        </li>
                     </ul>
                 </li>
 
@@ -70,7 +110,14 @@
                     <ul class="slide-menu child1">
                         <li class="slide side-menu__label1"><a href="javascript:void(0)">Marketing</a></li>
                         <li class="slide"><a href="marketing-create-invoice.html" class="side-menu__item">Generate Invoice</a></li>
-                        <li class="slide"><a href="marketing-invoice-history.html" class="side-menu__item">View Invoice Records</a></li>
+                        <li class="slide">
+                            <a href="ecommerce-invoice-history.html" class="side-menu__item d-flex align-items-center justify-content-between">
+                                <div>
+                                    <span>Connected Sites</span>
+                                </div>
+                                <span class="badge bg-success">34</span>
+                            </a>
+                        </li>
                     </ul>
                 </li>
 
@@ -86,7 +133,14 @@
                     <ul class="slide-menu child1">
                         <li class="slide side-menu__label1"><a href="javascript:void(0)">Gaming</a></li>
                         <li class="slide"><a href="gaming-create-invoice.html" class="side-menu__item">Generate Invoice</a></li>
-                        <li class="slide"><a href="gaming-invoice-history.html" class="side-menu__item">View Invoice Records</a></li>
+                        <li class="slide">
+                            <a href="ecommerce-invoice-history.html" class="side-menu__item d-flex align-items-center justify-content-between">
+                                <div>
+                                    <span>Connected Sites</span>
+                                </div>
+                                <span class="badge bg-success">34</span>
+                            </a>
+                        </li>
                     </ul>
                 </li>
 
@@ -102,7 +156,14 @@
                     <ul class="slide-menu child1">
                         <li class="slide side-menu__label1"><a href="javascript:void(0)">Content Writing</a></li>
                         <li class="slide"><a href="content-create-invoice.html" class="side-menu__item">Generate Invoice</a></li>
-                        <li class="slide"><a href="content-invoice-history.html" class="side-menu__item">View Invoice Records</a></li>
+                        <li class="slide">
+                            <a href="ecommerce-invoice-history.html" class="side-menu__item d-flex align-items-center justify-content-between">
+                                <div>
+                                    <span>Connected Sites</span>
+                                </div>
+                                <span class="badge bg-success">34</span>
+                            </a>
+                        </li>
                     </ul>
                 </li>
 
@@ -118,7 +179,14 @@
                     <ul class="slide-menu child1">
                         <li class="slide side-menu__label1"><a href="javascript:void(0)">Image Gallery</a></li>
                         <li class="slide"><a href="gallery-create-invoice.html" class="side-menu__item">Generate Invoice</a></li>
-                        <li class="slide"><a href="gallery-invoice-history.html" class="side-menu__item">View Invoice Records</a></li>
+                        <li class="slide">
+                            <a href="ecommerce-invoice-history.html" class="side-menu__item d-flex align-items-center justify-content-between">
+                                <div>
+                                    <span>Connected Sites</span>
+                                </div>
+                                <span class="badge bg-success">34</span>
+                            </a>
+                        </li>
                     </ul>
                 </li>
 
@@ -134,7 +202,14 @@
                     <ul class="slide-menu child1">
                         <li class="slide side-menu__label1"><a href="javascript:void(0)">Gift Card</a></li>
                         <li class="slide"><a href="giftcard-create-invoice.html" class="side-menu__item">Generate Invoice</a></li>
-                        <li class="slide"><a href="giftcard-invoice-history.html" class="side-menu__item">View Invoice Records</a></li>
+                        <li class="slide">
+                            <a href="ecommerce-invoice-history.html" class="side-menu__item d-flex align-items-center justify-content-between">
+                                <div>
+                                    <span>Connected Sites</span>
+                                </div>
+                                <span class="badge bg-success">34</span>
+                            </a>
+                        </li>
                     </ul>
                 </li>
 
@@ -150,10 +225,19 @@
                     <ul class="slide-menu child1">
                         <li class="slide side-menu__label1"><a href="javascript:void(0)" data-i18n="translation">Translation</a></li>
                         <li class="slide"><a href="translation-create-invoice.html" class="side-menu__item" data-i18n="create_invoice">Generate Invoice</a></li>
-                        <li class="slide"><a href="translation-invoice-history.html" class="side-menu__item" data-i18n="invoice_history">View Invoice Records</a></li>
+                        <li class="slide">
+                            <a href="ecommerce-invoice-history.html" class="side-menu__item d-flex align-items-center justify-content-between">
+                                <div>
+                                    <span>Connected Sites</span>
+                                </div>
+                                <span class="badge bg-success">34</span>
+                            </a>
+                        </li>
                     </ul>
                 </li>
+
                 <!-- End::slide -->
+
 
 
                 <!-- Start::slide__category -->
