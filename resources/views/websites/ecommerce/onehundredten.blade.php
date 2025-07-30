d01e00850c9a0cffa246f488c405a9f6af42c3ed
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2025-07-31 01:33:22 +0530

    4th trail to reoslve foote rissue

--- resources/views/websites/ecommerce/onehundredten.blade.php
+++ resources/views/websites/ecommerce/onehundredten.blade.php
@@ -92,7 +92,7 @@
                                 </td>
                              </tr>
                             </table>
-                            <div style="min-height: 500px !important;">
+                            <div style="min-height: 650px !important;">
                               <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse;margin-top: 40px;">
                                  <tr style="background:#021228">
                                  <td>
