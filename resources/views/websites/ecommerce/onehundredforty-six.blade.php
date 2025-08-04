88d9a6f61fe770a01631bfe15311b32d7c1f2bc7
vijay <gadgetzone@gadgetzones-MacBook-Air-2.local>
2025-08-04 12:24:52 +0530

    arrow srecve update again

--- resources/views/websites/ecommerce/onehundredforty-six.blade.php
+++ resources/views/websites/ecommerce/onehundredforty-six.blade.php
@@ -15,7 +15,7 @@
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
         <tr>
             <td align="center">
-                <table width="100%" height="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff"
+                <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff"
                     style="border-collapse: collapse; box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);">
                     <!-- Header -->
                     <tr>
