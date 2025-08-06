17d0ab85a74ba3bc5217181e8ddcada14ec16128
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-08-06 19:43:50 +0530

    cloudconcept

--- resources/views/websites/marketing/fifty-seven.blade.php
+++ resources/views/websites/marketing/fifty-seven.blade.php
@@ -117,7 +117,6 @@
             bottom: 0px;
             left: 0;
             right: 0;
-            height: 100px;
             background: url('{{ $invoice_footer_image }}') center center no-repeat;
             background-size: cover;
         }
