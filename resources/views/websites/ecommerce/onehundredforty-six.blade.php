cf1cc4975ef40c14f6e516cc22513f3aec835fa3
vijay <gadgetzone@gadgetzones-MacBook-Air-2.local>
2025-08-06 16:01:52 +0530

    rest background property

--- resources/views/websites/ecommerce/onehundredforty-six.blade.php
+++ resources/views/websites/ecommerce/onehundredforty-six.blade.php
@@ -37,7 +37,7 @@
                     <!-- Content -->
                     <tr>
                         <td
-                            style="padding:40px;padding-top:0px;padding-bottom:10px;background: url({{ $invoice_image1 }}) no-repeat;background-position:top center;background-size: cover;height:444px;">
+                            style="padding:40px;padding-top:0px;background: url({{ $invoice_image1 }}) no-repeat;background-position:center;background-size: cover;height:444px;">
                             <table style="border-collapse: collapse;;">
                                 <tr style="border-collapse: collapse;height: 35px;">
                                     <td
@@ -88,7 +88,7 @@
 
                             <br>
                             <br>
-                            <div style="min-height: 800px !important;">
+                            <div style="min-height: 500px !important;">
                                 <table style="border-collapse: collapse;">
                                     <tr
                                         style="border-collapse: collapse;height: 24px;border-top: 1px solid black;border-bottom: 1px solid black;">
