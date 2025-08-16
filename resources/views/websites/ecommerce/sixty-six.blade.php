c47c094d9412a3364fcac7de506e0522ada9b95e
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-08-16 16:13:15 +0530

    changed

--- resources/views/websites/ecommerce/sixty-six.blade.php
+++ resources/views/websites/ecommerce/sixty-six.blade.php
@@ -32,7 +32,7 @@
                         <td style="padding:40px;padding-top:0px;">
                             <table>
                                 <tr>
-                                    <td style="width:650px;text-align: center;">
+                                    <td style="width:900px;text-align: center;">
                                        <h1 style="color:#474748;text-align: center;margin: 0px;font-size: 36;font-family: Arial;">
                                         INVOICE
                                        </h1>
