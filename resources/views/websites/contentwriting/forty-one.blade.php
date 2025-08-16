a6c5c313532bd9d073164b3f1aa95d34295730bc
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-08-16 14:50:26 +0530

    changed

--- resources/views/websites/contentwriting/forty-one.blade.php
+++ resources/views/websites/contentwriting/forty-one.blade.php
@@ -85,7 +85,7 @@
                            <br>
                         <br>
                          <div style="min-height: 500px !important;">
-                            <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;border-bottom: 0px;border: 0px; width: 560px;">
+                            <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;border-bottom: 0px;border: 0px;">
                                 <tr style="border-collapse: collapse;height: 30px;border-bottom: 0px;border: 0px;border-bottom: 1px solid black;border-top: 1px solid black;" >
                                     <td style="width: 250px;text-align: left;font-family: arial;font-size: 10px;margin: 0px;font-weight: 400;border-collapse: collapse;">
                                        <b>SERVICE</b> 
