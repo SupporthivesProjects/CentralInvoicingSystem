573f2399f278d7f06493fc02151c67dc30b8fb0a
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-08-19 17:28:45 +0530

    changed

--- resources/views/websites/ecommerce/twohundredfive.blade.php
+++ resources/views/websites/ecommerce/twohundredfive.blade.php
@@ -50,7 +50,7 @@
                                 </tr>
                             </table>
                             
-                            <div style="min-height: 750px;">
+                            <div style="min-height: 790px;">
                                 <table
                                     style="margin-top: 70px; width: 100%; padding: 40px; border-collapse: collapse; color: white; font-size: 11px;">
                                     <thead>
