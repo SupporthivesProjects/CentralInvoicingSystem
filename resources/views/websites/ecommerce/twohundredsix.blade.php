fc7eb5ba10d8ed333a9b6409167b23ffa46be33c
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-08-19 16:23:34 +0530

    changed

--- resources/views/websites/ecommerce/twohundredsix.blade.php
+++ resources/views/websites/ecommerce/twohundredsix.blade.php
@@ -8,7 +8,7 @@
 <body style="margin: 0px; padding: 0px;">
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
         <tr>
-            <td align="center">
+            <td align="top">
                 <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff"
                     style="border-collapse: collapse; box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);">
                     <!-- Header -->
@@ -46,7 +46,7 @@
 
 
                     <!-- Content -->
-                     <div style="min-height: 400px;">
+                     <div style="min-height: 500px;">
                         <tr>
                             
                             <td style="padding:40px;padding-top:0px;background: url(img/body_bg.png) no-repeat;background-position: center;background-size: cover;height:444px; font-family: 'arial'; font-size: 9px; vertical-align: top;">
