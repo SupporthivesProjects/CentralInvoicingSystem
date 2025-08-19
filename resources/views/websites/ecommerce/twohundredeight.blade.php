b39a12a9b940d75ca277fb2ca5d7df6ba57322a7
gadgetzone <gadgetzone@tirthak-MacBook-Pro.local>
2025-08-19 18:57:42 +0530

    19august cis issues8

--- resources/views/websites/ecommerce/twohundredeight.blade.php
+++ resources/views/websites/ecommerce/twohundredeight.blade.php
@@ -16,7 +16,7 @@
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
         <tr>
             <td align="center">
-                <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse; box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);">
+                <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse;">
                     <!-- Header -->
                     <tr>
                         <td style="background: url('{{ $invoice_header_image }}');background-repeat: no-repeat;height:130px;background-size: cover;"></td>
@@ -39,7 +39,7 @@
                                     </td> 
                                   </tr>
                                   <tr style="height:40px;"></tr>
-                                  <tr style="height:570px;">
+                                  <tr style="height:580px;">
                                     <td style="width:70%;">
                                       <table style="border-collapse: collapse;width:100%;">
                                         <tr style="border-top: 1px solid #ffffff;border-bottom: 1px solid #ffffff;">
@@ -146,7 +146,7 @@
                     <!-- Content End-->
                     <!-----------Footer----------->
                     <tr>
-                        <td align="center" style="height: 200px;background:url('{{ $invoice_image2 }}');background-size: cover;background-repeat: no-repeat;">
+                        <td align="center" style="height: 210px;background:url('{{ $invoice_image2 }}');background-size: cover;background-repeat: no-repeat;">
                             <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;"> 
                                 <tr>
                                     <td style="padding:40px 40px 100px 40px;">
