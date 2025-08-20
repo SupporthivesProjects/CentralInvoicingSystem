726d54e5aafd4f5252cd33c307ec8c6fb4567a33
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2025-08-20 12:05:04 +0530

    testing dev team

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -12,7 +12,7 @@
 
                 <div class="d-md-flex d-block align-items-center justify-content-between page-header-breadcrumb">
                     <div>
-                        <h2 class="main-content-title fs-24 mb-1">Welcome To0 Dashboard</h2>
+                        <h2 class="main-content-title fs-24 mb-1">Welcome To Dashboard</h2>
                         <ol class="breadcrumb mb-0">
                             <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Central Invoice System</li>
