254ca34ec2b4ae38117f11ff553cf6b971854100
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-08-22 09:17:26 +0530

    change

--- resources/views/websites/ecommerce/onehundredseventeen.blade.php
+++ resources/views/websites/ecommerce/onehundredseventeen.blade.php
@@ -45,7 +45,7 @@
                             </table>
                         </td>
                     </tr>
-                    <tr style="display: flex;">
+                    <tr style="display: flex; height: 80vh;">
                         <td style="padding:0px 40px 60px 24px;height:650px;">
                             <br>
                             <br>
