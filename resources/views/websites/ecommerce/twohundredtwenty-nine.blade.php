5d9e622cab83ea2bc7b5121e608d9945311b73c3
saakshichat <saakshi@supporthives.com>
2025-08-22 11:40:46 +0530

    229

--- resources/views/websites/ecommerce/twohundredtwenty-nine.blade.php
+++ resources/views/websites/ecommerce/twohundredtwenty-nine.blade.php
@@ -13,7 +13,7 @@
         <tr>
             <td align="center" bgcolor="#f2f2f2" style="padding:0px;">
                 <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff"
-                    style="border-collapse: collapse; box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1); background: url({{ $invoice_image1 }}) no-repeat;background-position:center;background-size:100% 100%;">
+                    style="border-collapse: collapse; box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1); background: url({{ $invoice_image1 }}) no-repeat;background-position:center;background-size:100% 100%;background-size:cover;">
                     <!-- Header -->
                     <tr>
                         <td style="height: 20px;"></td>
