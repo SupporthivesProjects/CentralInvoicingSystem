fb57212651ffcc4fb82edd879f87109d456e223e
saakshichat <saakshi@supporthives.com>
2025-08-23 17:59:06 +0530

    devlabs

--- resources/views/websites/ecommerce/twohundredtwenty-four.blade.php
+++ resources/views/websites/ecommerce/twohundredtwenty-four.blade.php
@@ -17,7 +17,7 @@
     <table align="center" width="100%" cellpadding="0" cellspacing="0">
         <tr>
             <td
-                style="color: #000;border-radius: 10px; overflow: hidden; background: url('{{ $invoice_image1 }}') no-repeat; background-size:100% 100%;background-position:center;height:100vh;">
+                style="color: #000;border-radius: 10px; overflow: hidden; background: url('{{ $invoice_image1 }}') no-repeat; background-size:100% 100%;background-position:center;height:100%;">
                 <table align="center" width="100%" cellpadding="0" cellspacing="0" width="100%">
                     <!-- Header -->
                     <tr>
