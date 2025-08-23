d6009e118c673dd9f896218f589aa9a3dfa2cd43
saakshichat <saakshi@supporthives.com>
2025-08-23 14:54:33 +0530

    dialectlink

--- resources/views/websites/translation/twohundredtwenty-five.blade.php
+++ resources/views/websites/translation/twohundredtwenty-five.blade.php
@@ -19,7 +19,7 @@
         <tr>
             <td align="center" style="vertical-align:">
                 <!-- White Box Container -->
-                <table width="80%" cellpadding="0" cellspacing="0" style="margin-top:100px;height:83vh;">
+                <table width="80%" cellpadding="0" cellspacing="0" style="margin-top:100px;height:85vh;">
 
                     <!-- Invoice Header -->
                     <tr>
