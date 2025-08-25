28077e6ec154f0295998c367c17f37e17582b359
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-08-25 14:09:26 +0530

    c

--- resources/views/websites/translation/twohundredtwenty-three.blade.php
+++ resources/views/websites/translation/twohundredtwenty-three.blade.php
@@ -138,7 +138,7 @@
                     <!-----------Footer----------->
                     <tr>
                         <td>
-                            <table width="100%" style="padding: 0px 40px 0px 40px; font-family: 'Inter'; margin-top: -10px; background-color: #FFFFFF; box-shadow: none; height: 75px;">
+                            <table width="100%" style="padding: 0px 40px 0px 40px; font-family: 'Inter'; margin-top: -10px; background-color: #FFFFFF; box-shadow: none; height: 85px;">
                                 <tr>
                                     <td width="50%" style="vertical-align: bottom;">
                                         <p style="font-size: 10px;">{{ $company_email }}</p>
