efae27a0c1e9a5285b9d292c064528da58552db4
saakshichat <saakshi@supporthives.com>
2025-09-19 14:29:43 +0530

    new one

--- resources/views/websites/translation/twohundredtwenty-five.blade.php
+++ resources/views/websites/translation/twohundredtwenty-five.blade.php
@@ -19,7 +19,7 @@
         <tr>
             <td align="center" style="vertical-align:">
                 <!-- White Box Container -->
-                <table width="80%" cellpadding="0" cellspacing="0" style="margin-top:120px;height:91.1vh;vertical-align:top;">
+                <table width="80%" cellpadding="0" cellspacing="0" style="margin-top:120px;height:80vh;">
 
                     <!-- Invoice Header -->
                     <tr>
