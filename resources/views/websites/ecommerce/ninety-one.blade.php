5d404a1a91cd698c029571c56b1230d379fc63bf
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-09-25 17:35:34 +0530

    greenpanda

--- resources/views/websites/ecommerce/ninety-one.blade.php
+++ resources/views/websites/ecommerce/ninety-one.blade.php
@@ -40,7 +40,7 @@
                         <td style="padding:0px;">
                             <div style="min-height: 1200px !important;">
                             <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;min-height: 1200px !important;">
-                                <tr style="min-height: 1200px !important;">
+                                <tr style="min-height: 1200px !important;background: red;">
                                     <td style="width:20px;"></td>
                                     <td align="center" style="vertical-align: top;">
                                        <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;">
