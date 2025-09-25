c34ac3a1b7ec06bb347e4af4918af7feeffbabaf
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-09-25 17:46:12 +0530

    greenpanda

--- resources/views/websites/ecommerce/ninety-one.blade.php
+++ resources/views/websites/ecommerce/ninety-one.blade.php
@@ -38,9 +38,9 @@
                     <!-- Content -->
                     <tr style="background:#ffff ;">
                         <td style="padding:0px;">
-                            <div style="min-height: 1050px !important;">
-                            <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;min-height: 1050px !important;">
-                                <tr style="min-height: 1050px !important;">
+                            <div style="min-height: 1020px !important;">
+                            <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;min-height: 1020px !important;">
+                                <tr style="min-height: 1020px !important;">
                                     <td style="width:20px;"></td>
                                     <td align="center" style="vertical-align: top;">
                                        <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;">
