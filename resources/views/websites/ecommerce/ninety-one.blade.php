a49dccb368e24c2aca2ee8578848361ea0cb28b3
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-09-25 17:26:46 +0530

    greenpanda

--- resources/views/websites/ecommerce/ninety-one.blade.php
+++ resources/views/websites/ecommerce/ninety-one.blade.php
@@ -39,15 +39,15 @@
                     <tr style="background:#ffff ;">
                         <td style="padding:0px;">
                             <div style="min-height: 900px !important;">
-                            <table width="100%" style="min-height: 900px !important;" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;">
-                                <tr style="min-height: 1000px !important; background: red;">
+                            <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;">
+                                <tr>
                                     <td style="width:20px;"></td>
                                     <td align="center" style="vertical-align: top;">
                                        <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;">
                                         <tr style="border-top: 1px solid black;border-bottom:2px solid black;height:30px;">
                                             <td style="width: 50%;padding-left: 10px;">
                                                  <p style="margin: 0px;font-family:Source Sans Pro;font-size:9px;color: #000000;">
-                                                  Product
+                                                  Productssss
                                                 </p>
                                             </td>
                                             <td style="width:20%;" align="center">
