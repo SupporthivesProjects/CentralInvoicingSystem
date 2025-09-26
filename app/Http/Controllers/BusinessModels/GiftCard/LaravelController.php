a1916c4ea2dc6ac4ea33a657ab94214b576f7f71
Narayan Zade <narayan.d.zade@gmail.com>
2025-09-26 14:31:08 +0530

    remove single product condition

--- app/Http/Controllers/BusinessModels/GiftCard/LaravelController.php
+++ app/Http/Controllers/BusinessModels/GiftCard/LaravelController.php
@@ -90,18 +90,27 @@ class LaravelController extends Controller
 
         $bestMatch = null;
         $bestTotal = 0;
-    
-        if (mt_rand(1, 100) <= 40) {
 
-            $checkSingleProductQuery = clone $subQuery;
-            $matchingProducts = $checkSingleProductQuery->where('unit_price', $invoiceAmount)->get();
-        
-            if ($matchingProducts->isNotEmpty()) {
-                $randomProduct = $matchingProducts->shuffle()->first();
-                $bestMatch = collect([$randomProduct]);
-                $bestTotal = $invoiceAmount;
-            }
+        $checkSingleProductQuery = clone $subQuery;
+        $matchingProducts = $checkSingleProductQuery->where('unit_price', $invoiceAmount)->get();
+    
+        if ($matchingProducts->isNotEmpty()) {
+            $randomProduct = $matchingProducts->shuffle()->first();
+            $bestMatch = collect([$randomProduct]);
+            $bestTotal = $invoiceAmount;
         }
+    
+        // if (mt_rand(1, 100) <= 40) {
+
+        //     $checkSingleProductQuery = clone $subQuery;
+        //     $matchingProducts = $checkSingleProductQuery->where('unit_price', $invoiceAmount)->get();
+        
+        //     if ($matchingProducts->isNotEmpty()) {
+        //         $randomProduct = $matchingProducts->shuffle()->first();
+        //         $bestMatch = collect([$randomProduct]);
+        //         $bestTotal = $invoiceAmount;
+        //     }
+        // }
     
         if (!$bestMatch) {
             $bestDistance = PHP_INT_MAX;
