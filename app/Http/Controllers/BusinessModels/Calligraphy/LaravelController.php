134aa30eb2026a7700b7eb07ca3636b7049124f8
Narayan Zade <narayan.d.zade@gmail.com>
2025-09-29 11:09:37 +0530

    changes

--- app/Http/Controllers/BusinessModels/Calligraphy/LaravelController.php
+++ app/Http/Controllers/BusinessModels/Calligraphy/LaravelController.php
@@ -588,7 +588,6 @@ class LaravelController extends Controller
 
         $modelType = $site->businessModel->model_type;
         $random_amount = session('current_amount', 0);
-
         $tableRows = view( "invoice.{$modelType}.add_product_rows", ['products' => $products, 'site' => $site,'random_amount' => $random_amount])->render();
         $paginationHtml = view("invoice.{$modelType}.pagination", ['totalPages' => $totalPages,'paginationPages' => $paginationPages, 'currentPage' => $page ])->render();
 
