178247a0b15e6972e6327244e69911da95d220de
gadgetzone <gadgetzone@tirthak-MacBook-Pro.local>
2025-10-12 00:40:07 +0530

    erfscd

--- resources/views/websites/ecommerce/onehundredfifty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredfifty-seven.blade.php
@@ -42,7 +42,7 @@
                                     <tr>
                                         <td colspan="1" style="padding: 10px; background-color: #f1e9df; font-weight: bold">BILL FROM</td>
                                         <td colspan="1" style="padding: 10px; background-color: #ffffff; font-weight: bold ; min-width: 30px"></td>
-                                        <td colspan="2" style="padding: 10px; background-color: #f1e9df; font-weight: bold">BILL TO</td>
+                                        <td colspan="3" style="padding: 10px; background-color: #f1e9df; font-weight: bold">BILL TO</td>
                                     </tr>
                                     <tr>
                                         <td colspan="1" style="padding: 10px; border-bottom: 1px solid #ddd; font-size: 14px">
@@ -52,7 +52,7 @@
                                             <strong>Email :</strong> {{ $company_email }}
                                         </td>
                                         <td colspan="1" style="padding: 10px; border-bottom: 1px solid #ddd; font-size: 14px; min-width: 30px"></td>
-                                        <td colspan="2" style="padding: 10px; border-bottom: 1px solid #ddd; font-size: 14px">
+                                        <td colspan="3" style="padding: 10px; border-bottom: 1px solid #ddd; font-size: 14px">
                                             <strong>{{ $customer_name }}</strong><br />
                                             <!-- <strong>Phone :</strong> {{ $customer_mobile }}<br />
                                             <strong>Email :</strong> {{ $customer_email }} -->
