b6b0dfbcf991d63194649c026a90586a55151fe0
swarali <Swarali@supporthives.com>
2025-10-15 17:58:28 +0530

    price total fix

--- resources/views/websites/ecommerce/twohundredfive.blade.php
+++ resources/views/websites/ecommerce/twohundredfive.blade.php
@@ -75,7 +75,7 @@
                                             </td>
                                             <td style="padding: 12px; text-align: right; border-bottom: 1px solid #ccc;">
                                                 {{-- site_currency() . number_format($invoice_amount ?? 0, 2) --}}
-                                                {{ site_currency() . number_format($product->unit_price ?? 0, 2) }}
+                                                {{ site_currency() . number_format($product->unit_price, 2) }}
                                             </td>
                                         </tr>
                                         @endforeach
