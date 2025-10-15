ffd7c4f5542f52c538a5a607fb0469702d71d003
gadgetzone <gadgetzone@tirthak-MacBook-Pro.local>
2025-10-15 14:38:23 +0530

    fsc

--- resources/views/websites/ecommerce/twohundredtwenty-nine.blade.php
+++ resources/views/websites/ecommerce/twohundredtwenty-nine.blade.php
@@ -59,26 +59,24 @@
 
 
                                 <tr style="border-collapse: collapse;">
-                                    <td style="padding-top: 10px;width: 300px;vertical-align: top;">
+                                    <td style="padding-top: 10px;width: 50%;vertical-align: top;">
                                         <p
                                             style="font-family: arial;font-size: 14px;margin: 0px;font-weight: 400; text-align: left;margin-bottom: 8px;padding-left: 5px;color: darkgreen;">
                                             <b>Invoice to</b>
                                         </p>
-                                        <br>
                                         <p
-                                            style="font-family: arial;font-size: 12px;margin: 0px;font-weight: 400; text-align: left;margin-bottom: 8px;padding-left: 5px;">
+                                            style="font-family: arial;font-size: 20px;margin: 0px;font-weight: 400; text-align: left;margin-bottom: 8px;padding-left: 5px;">
                                             {{ $customer_name }}
                                         </p>
                                     </td>
 
-                                    <td style="padding-top: 10px;width: 300px;vertical-align: top;">
+                                    <td style="padding-top: 10px;width: 50%;vertical-align: top;">
                                         <p
                                             style="font-family: arial;font-size: 14px;margin: 0px;font-weight: 400; text-align: left;margin-bottom: 8px;padding-right: 5px;color: darkgreen;">
                                             <b>Billed from:</b>
                                         </p>
-                                        <br>
                                         <p
-                                            style="font-family: arial;font-size: 12px;margin: 0px;font-weight: 400;text-align: left; margin-bottom: 8px;padding-left: 5px;">
+                                            style="font-family: arial;font-size: 20px;margin: 0px;font-weight: 400;text-align: left; margin-bottom: 8px;padding-left: 5px;">
                                             {{ $company_name }}
                                         </p>
                                         <p
