5bde402eacc89d8bc75eb6d20357cb07750ddf07
gadgetzone <gadgetzone@tirthak-MacBook-Pro.local>
2025-10-16 13:16:08 +0530

    rwfsdC

--- resources/views/websites/gaming/onehundredthirty-one.blade.php
+++ resources/views/websites/gaming/onehundredthirty-one.blade.php
@@ -121,7 +121,7 @@
                                         </p>
                                     </td>
                                     <td style="width: 60px;"></td>
-                                    <td style="vertical-align: top; padding-left:110px">
+                                    <td style="vertical-align: top; padding-left:105px">
                                         <h1
                                             style="margin: 0px;font-family: Lato;font-size: 12px;color: #4D4D4D;line-height:18px;">
                                             Bill To
