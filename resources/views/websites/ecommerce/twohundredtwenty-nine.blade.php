8582a1b2b5f6f115b5fd8f6053d5f1b08c7f3dbc
gadgetzone <gadgetzone@tirthak-MacBook-Pro.local>
2025-10-16 12:58:18 +0530

    erfscdzX

--- resources/views/websites/ecommerce/twohundredtwenty-nine.blade.php
+++ resources/views/websites/ecommerce/twohundredtwenty-nine.blade.php
@@ -177,7 +177,7 @@
                                     <td colspan="5"
                                         style="border-top-left-radius: 10px;border-bottom-left-radius: 10px; width: 100%;font-weight: 400; border-collapse: collapse;padding-left: 2px;  text-align: right;padding-right: 20px;">
                                         <!-- <div style="display: flex; flex-direction: row; justify-content: flex-end;"> -->
-                                            <div style="display:  inline-flex; flex-direction: row;justify-content: space-around;gap: 8px;align-items: center;background-color: white;width: 500px; border-radius:10px; height: 40px, ">
+                                            <div style="display:  inline-flex; flex-direction: row;justify-content: space-around;gap: 8px;align-items: center;background-color: white;width: 500px; border-radius:10px; height: 40px ">
                                                 <span style="font-size: 20px; color: black;">•</span>
                                                 <b style="color: darkgreen; text-align: center;font-family: 'Space Mono', monospace;font-size: 11px;margin: 0px;">SUBTOTAL</b>
                                                 <p style="color: darkgreen;">{{ site_currency() }} {{ number_format(($invoice_amount + $discount_amount), 2) }}</p>
