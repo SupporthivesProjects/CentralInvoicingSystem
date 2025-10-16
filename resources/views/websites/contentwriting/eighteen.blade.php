a3c379dbef90cd4a9701a7cb05443795a3fb6e00
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-10-16 11:45:03 +0530

    change

--- resources/views/websites/contentwriting/eighteen.blade.php
+++ resources/views/websites/contentwriting/eighteen.blade.php
@@ -82,7 +82,7 @@
                             </tr>
                         </table>
 
-                        <div style="min-height: 650px;">
+                        <div style="min-height: 660px;">
                             <table width="100%"  style="border-collapse: collapse;margin-top:30px;min-height: 450px !important;">
                             <tr style="background-color: #FF444E !important; height: 30px;">
                                 <td>
