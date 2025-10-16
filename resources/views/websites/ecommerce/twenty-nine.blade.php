f256abcc0d63451ffc97758b5faac09fa61894c3
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-10-16 10:56:54 +0530

    change

--- resources/views/websites/ecommerce/twenty-nine.blade.php
+++ resources/views/websites/ecommerce/twenty-nine.blade.php
@@ -34,7 +34,7 @@
 
                      <!-- Content -->
                     <tr style="background: url('{{ $invoice_image4 }}') no-repeat;background-position: center;background-size: cover;">
-                        <td style="padding: 70px;padding-top:30px;">
+                        <td style="padding: 70px;padding-top:26px;">
                             <table>
                                 <br>
                                 <tr>
