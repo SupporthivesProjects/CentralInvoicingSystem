2b53d4cc5997b26b59092c29c9c6d63340863e2d
swarali <Swarali@supporthives.com>
2025-10-16 12:58:23 +0530

    new foreach

--- resources/views/websites/ecommerce/twohundredfive.blade.php
+++ resources/views/websites/ecommerce/twohundredfive.blade.php
@@ -67,18 +67,19 @@
                                     </thead>
                                     <tbody style="color: white;">
                                         @foreach ($products as $product)
-                                        <tr>
-                                            <td style="padding: 12px; border-bottom: 1px solid #ccc;">{{ $product->name }}</td>
-                                            <td style="padding: 12px; text-align: right; border-bottom: 1px solid #ccc;">
-                                                {{ site_currency() . number_format($product->unit_price ?? 0, 2) }}</td>
-                                            <td style="padding: 12px; text-align: center; border-bottom: 1px solid #ccc;">1
-                                            </td>
-                                            <td style="padding: 12px; text-align: right; border-bottom: 1px solid #ccc;">
-                                                {{-- site_currency() . number_format($invoice_amount ?? 0, 2) --}}
-                                                {{ site_currency() . number_format($product->unit_price ?? 0, 2) }}
-                                            </td>
-                                        </tr>
+                                            <tr>
+                                                <td style="padding: 12px; border-bottom: 1px solid #ccc;">{{ $product->name }}</td>
+                                                <td style="padding: 12px; text-align: right; border-bottom: 1px solid #ccc;">
+                                                    {{ site_currency() }} {{  number_format($product->unit_price ?? 0, 2) }}</td>
+                                                <td style="padding: 12px; text-align: center; border-bottom: 1px solid #ccc;">1
+                                                </td>
+                                                <td style="padding: 12px; text-align: right; border-bottom: 1px solid #ccc;">
+                                                    {{ site_currency() }} {{ number_format($product->unit_price, 2) }}
+                                                    
+                                                </td>
+                                            </tr>
                                         @endforeach
+
                                         {{-- <tr>
                                             <td style="padding: 12px; border-bottom: 1px solid #ccc;">Item Name</td>
                                             <td style="padding: 12px; text-align: right; border-bottom: 1px solid #ccc;">
