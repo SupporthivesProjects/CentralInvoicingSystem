9e2f88e1f53434ff6b42427db7d0689b6e952ae7
tirthak <gadgetzone@tirthak-MacBook-Pro.local>
2025-10-17 01:19:31 +0530

    wdc

--- resources/views/websites/ecommerce/onehundredsixty.blade.php
+++ resources/views/websites/ecommerce/onehundredsixty.blade.php
@@ -23,10 +23,10 @@
                 <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse;">
                     <!-- Header -->
                     <tr style="width: 100%;">
-                        <td style="padding: 0px;max-height: 180px;width: 100%;">
+                        <td style="padding: 0px;max-height: 230px;width: 100%;">
                             <table style="width: 100%;">
                                 <tr style="width: 100%;">
-                                    <td style="height: 180px; background: url({{ $invoice_header_image }}) no-repeat;background-position: top center;background-size:cover;width: 100%;">
+                                    <td style="height: 230px; background: url({{ $invoice_header_image }}) no-repeat;background-position: top center;background-size:cover;width: 100%;">
                                         <b style="padding-left: 120px;color: white;font-size: 22px;font-family: 'Courier New', Courier, monospace;"> Invoice</b><br>
                                         <b style="padding-left: 120px;color: white;font-size: 9px;font-family: 'Courier New', Courier, monospace;">No.{{ $invoice_number }}</b>
                                     </td>
