f5928c3171398db18ba4922ec17749baf8084ed3
tirthak <gadgetzone@tirthak-MacBook-Pro.local>
2025-10-17 01:23:11 +0530

    wrdsc'

--- resources/views/websites/ecommerce/onehundredsixty.blade.php
+++ resources/views/websites/ecommerce/onehundredsixty.blade.php
@@ -27,8 +27,8 @@
                             <table style="width: 100%;">
                                 <tr style="width: 100%;">
                                     <td style="height: 230px; background: url({{ $invoice_header_image }}) no-repeat;background-position: top center;background-size:cover;width: 100%;">
-                                        <b style="padding-left: 120px;color: white;font-size: 22px;font-family: 'Courier New', Courier, monospace;"> Invoice</b><br>
-                                        <b style="padding-left: 120px;color: white;font-size: 9px;font-family: 'Courier New', Courier, monospace;">No.{{ $invoice_number }}</b>
+                                        <b style="padding-left: 90px;color: white;font-size: 22px;font-family: 'Courier New', Courier, monospace;"> Invoice</b><br>
+                                        <b style="padding-left: 92px;color: white;font-size: 9px;font-family: 'Courier New', Courier, monospace;">No.{{ $invoice_number }}</b>
                                     </td>
 
                                 </tr>
