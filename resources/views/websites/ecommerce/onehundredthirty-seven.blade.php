c5663882fe0c9009c07d48c46def7a29c6867cd6
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-10-22 14:15:41 +0530

    change

--- resources/views/websites/ecommerce/onehundredthirty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredthirty-seven.blade.php
@@ -82,15 +82,19 @@
                                             </p>
                                         @endif
 
-                                        @if(!empty($company_address))
+                                        <!-- @if(!empty($company_address))
                                             <p style="margin: 0px;font-family: Arial;font-size: 10px;line-height: 18px;">
+                                                <b>Address : </b>{{ $company_address }}
+                                            </p>
+                                        @endif -->
+
+                                        <p style="margin: 0px;font-family: Arial;font-size: 10px;line-height: 18px;">
                                                 <b>Address : </b><br>
                                                 KHAZANOVA STRATEGIC EXPERIENCES<br>
                                                 Meydan Grandstand,<br><br>
                                                 6th floor  Meydan Road,<br><br>
                                                 Nad Al Sheba  Dubai, U.A.E
                                             </p>
-                                        @endif
 
                                     </td>
                                 </tr>
