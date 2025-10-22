b77f816acbbdd2428475053f4d8d24a328476478
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-10-22 13:16:52 +0530

    160 2

--- resources/views/websites/ecommerce/onehundredsixty.blade.php
+++ resources/views/websites/ecommerce/onehundredsixty.blade.php
@@ -19,9 +19,9 @@
     </style>
 </head>
 <body>
-    <table width="100%" cellspacing="0" cellpadding="0" border="0" style="border-collapse: collapse; padding: 0px; margin: 0px;">
+    <!-- <table width="100%" cellspacing="0" cellpadding="0" border="0" style="border-collapse: collapse; padding: 0px; margin: 0px;">
         <tr style="width: 100%;">
-            <td bgcolor="#ffffff" style="width: 100%;">
+            <td bgcolor="#ffffff" style="width: 100%;"> -->
                 <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse;border-collapse: collapse; padding: 0px; margin: 0px;">
                     <!-- Header -->
                     <tr style="width: 100%;">
@@ -201,8 +201,8 @@
                     <!-----------Footer End----------->
 
                 </table>
-            </td>
+            <!-- </td>
         </tr>
-    </table>
+    </table> -->
 </body>
 </html>
