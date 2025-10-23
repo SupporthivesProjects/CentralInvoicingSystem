dd2d481f369763c7c96a6b7d8fc43328db09568d
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2025-10-23 17:24:16 +0530

    adding 308 code

---
+++ resources/views/websites/ecommerce/twohundredthirty-eight.blade.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        *{
+            margin: 0px;
+            padding: 0px;
+        }
+    </style>
+    <title>Your Email Title</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <table width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tr>
+            <td align="center" style="padding:0px;">
+                <table width="100%" cellspacing="0" cellpadding="0" border="0" style="border-collapse: collapse;background:url(img/background.png);background-size:100% 100%;background-position:center;background-repeat:no-repeat;">
+                    <!-- Header -->
+                    <tr>
+                        <td style="padding-left:72px;" colspan="2">
+                            <table style="margin-top:110px">
+                                <tr style="height: 20px;">
+                                    <td style="padding: 5px;">
+                                        <p style="font-size:9px;font-weight:400;font-family:Urbanist;margin: 0px;line-height:16px;text-align:left;color:white;">
+                                            Invoice No.
+                                        </p>
+                                    </td>
+                                    <td style="padding: 5px;">
+                                      <p style="font-size:9px;font-weight:400;font-family:Urbanist;margin: 0px;line-height:16px;text-align:left;color:white;">
+                                            123456789
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr style="height: 20px;">
+                                    <td style="padding: 5px;">
+                                        <p style="font-size:9px;font-weight:400;font-family:Urbanist;margin: 0px;line-height:16px;text-align:left;color:white;">
+                                            Date:
+                                        </p>
+                                    </td>
+                                    <td style="padding: 5px;">
+                                      <p style="font-size:9px;font-weight:400;font-family:Urbanist;margin: 0px;line-height:16px;text-align:left;color:white;">
+                                            April 10, 2025
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr style="height: 20px;"></tr>
+                                <tr style="height: 20px;">
+                                    <td style="padding: 5px;">
+                                        <p style="font-size:9px;font-weight:400;font-family:Urbanist;margin: 0px;line-height:16px;text-align:left;color:white;">
+                                            Invoice To:
+                                        </p>
+                                    </td>
+                                    <td style="padding: 5px;">
+                                      <p style="font-size:9px;font-weight:400;font-family:Urbanist;margin: 0px;line-height:16px;text-align:left;color:white;">
+                                            John Smith
+                                        </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <!-- Header End -->
+
+
+                    <!-- Content -->
+                    <tr>
+                        <td style="padding:20px;padding-bottom: 0px;width:80%" align="center">  
+                            <table cellspacing="0" cellpadding="0" border="0" width="100%" style="border-collapse: collapse;margin-top:40px;">
+                                <tr style="height:50px;border-bottom:1px solid black;">
+                                    <td style="width:40%;">
+                                        <p style="font-size: 10px;font-weight:700;font-family:Urbanist;margin: 0px;line-height: 28px;text-align: left;padding-left:10px;text-transform:uppercase;">
+                                            ITEM DESCRIPTION
+                                        </p>
+                                    </td>
+                                    <td style="width:20%;">
+                                        <p style="font-size: 10px;font-weight:700;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-right:10px;text-transform:uppercase;">
+                                            UNIT PRICE
+                                        </p>
+                                    </td>
+                                    <td style="width:20%;">
+                                        <p style="font-size: 10px;font-weight:700;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-right:10px;text-transform:uppercase;">
+                                            QTY
+                                        </p>
+                                    </td>
+                                    <td style="width:20%;">
+                                        <p style="font-size: 10px;font-weight:700;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-right:10px;text-transform:uppercase;">
+                                            Total
+                                        </p>
+                                    </td>
+                                </tr>
+                               
+                                <tr style="height:50px;border-bottom:1px solid black;">
+                                    <td>
+                                        <p style="font-size:8px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align: left;padding-left:10px;">
+                                            Item Name
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           4
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           $100.00
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family: Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           $400.00
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr style="height:50px;border-bottom:1px solid black;">
+                                    <td>
+                                        <p style="font-size:8px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align: left;padding-left:10px;">
+                                            Item Name
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           4
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           $100.00
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family: Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           $400.00
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr style="height:50px;border-bottom:1px solid black;">
+                                    <td>
+                                        <p style="font-size:8px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align: left;padding-left:10px;">
+                                            Item Name
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           4
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           $100.00
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family: Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           $400.00
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr style="height:50px;border-bottom:1px solid black;">
+                                    <td>
+                                        <p style="font-size:8px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align: left;padding-left:10px;">
+                                            Item Name
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           4
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family:Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           $100.00
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p style="font-size: 11px;font-weight: 500;font-family: Urbanist;margin: 0px;line-height:16px;text-align:center;padding-right:10px;">
+                                           $400.00
+                                        </p>
+                                    </td>
+                                </tr>
+                              <tr style="height:50px;">
+                                   <td colspan="2" ></td>
+                                    <td style="text-align:center;border-bottom:1px solid grey ;">
+                                        <p style="font-size:9px;font-weight:700;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-left:10px;text-transform:capitalize;">
+                                            Sub total
+                                        </p>
+                                    </td>
+                                    <td style="text-align:center;border-bottom:1px solid grey ;">
+                                        <p style="font-size:9px;font-weight:500;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-right:10px;text-transform: uppercase;">
+                                           900.00
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr style="height:50px;">
+                                   <td colspan="2" ></td>
+                                    <td style="text-align:center;border-bottom:1px solid grey ;">
+                                        <p style="font-size:9px;font-weight:700;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-left:10px;text-transform:capitalize;">
+                                            Discount
+                                        </p>
+                                    </td>
+                                    <td style="text-align:center;border-bottom:1px solid grey ;">
+                                        <p style="font-size:9px;font-weight:500;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-right:10px;text-transform: uppercase;color:rgb(137, 240, 133);">
+                                           -$25.00
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr style="height:50px;">
+                                   <td colspan="2" ></td>
+                                    <td style="text-align:center;border-bottom:1px solid grey ;">
+                                        <p style="font-size:11px;font-weight:700;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-left:10px;text-transform:capitalize;">
+                                            Grand Total
+                                        </p>
+                                    </td>
+                                    <td style="text-align:center;border-bottom:1px solid grey ;">
+                                        <p style="font-size:11px;font-weight:500;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-right:10px;text-transform: uppercase;">
+                                           $1,100.00
+                                        </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td style="width: 20%;"></td>
+                    </tr>
+                    <!-- Content End-->
+
+
+                    <!-----------Footer----------->
+                    <tr>
+                        <td align="center" style="height:50px;" colspan="2">
+                            <table width="100%" cellspacing="0" cellpadding="" border="0px" style="border-collapse: collapse;"> 
+                                <tr>
+                                    <td style="padding: 20px 40px;">
+                                    <h4 style="font-size:10px;font-weight:700;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:left;padding-left:10px;text-transform:capitalize;">
+                                    Contact Details
+                                   </h4>
+                                  <h4 style="font-size:10px;font-weight:400;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:left;padding-left:10px;text-transform:capitalize;">
+                                    support@ignitefitness.com <br>
+                                    Oasis Journey World Trading L.L.C-FZ, <br>
+                                    Meydan Grandstand, 6th floor, Meydan Road, Nad Al <br>
+                                    Sheba, Dubai, U.A.E.
+                                   </h4>
+                                    </td>
+                                </tr>              
+                            </table>
+                        </td>
+                    </tr> 
+                    <!-----------Footer End----------->    
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
