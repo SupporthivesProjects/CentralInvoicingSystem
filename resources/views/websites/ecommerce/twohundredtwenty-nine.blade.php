ccf64763b0cfd7f6189eaecfcc02349d7b41956b
KadamSaakshi <sakshi.k@supporthives.com>
2025-10-24 19:09:42 +0530

    229

--- resources/views/websites/ecommerce/twohundredtwenty-nine.blade.php
+++ resources/views/websites/ecommerce/twohundredtwenty-nine.blade.php
@@ -79,15 +79,15 @@
                                             {{ $company_name }}
                                         </p>-->
                                         <p style="font-family: arial;font-size: 14px;margin: 0;font-weight: 400;text-align: left;padding-bottom: 5px;color: darkgreen;">                               
-                                            <b> Address:</b>  <span style=" color: black;margin-left: 10px">{{ $company_address }} </span>
+                                            <b> Address:</b> <span style=" color: black;margin-left: 10px">{{ $company_address }} </span>
                                         </p>
                                         <p
                                             style="font-family: arial;font-size: 14px;margin: 0px;font-weight: 400; text-align: left;padding-bottom: 5px;padding-right: 5px;color: darkgreen;">
-                                            <b> Website:</b>  <span style="color: black;margin-left: 10px">{{ $site_name }}</span>
+                                            <b> Website:</b> <span style="color: black;margin-left: 10px">{{ $site_name }}</span>
                                         </p>
                                         <p
                                             style="font-family: arial;font-size: 14px;margin: 0px;font-weight: 400; text-align: left;padding-bottom: 5px;padding-right: 5px;color: darkgreen;">
-                                            <b> Email:</b>  <span style="color: black;margin-left: 10px">{{ $company_email }}</span>
+                                            <b> Email:</b> <span style="color: black;margin-left: 10px">{{ $company_email }}</span>
                                         </p>
                                     </td>
                                 </tr>
