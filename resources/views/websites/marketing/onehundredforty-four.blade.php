af7ecb9ca05eb141744ce7f03f40747e44dec766
tirthak <gadgetzone@tirthak-MacBook-Pro.local>
2025-10-24 21:50:28 +0530

    tgfv

--- resources/views/websites/marketing/onehundredforty-four.blade.php
+++ resources/views/websites/marketing/onehundredforty-four.blade.php
@@ -74,10 +74,10 @@
                                 </tr> -->
                                 <tr style="width:100%">
                                     <td style="width:100%">
-                                        <div style="display:flex; align-items: flex-end; flex-direction: column; gap: 8;width:100%;  padding-top: 44px; padding-left:30px;">
+                                        <div style="display:flex; align-items: flex-end; flex-direction: column; gap: 8;width:100%;  padding-top: 44px; padding-left:70px;">
                                             <div style="display:flex; align-items: flex-start; flex-direction: column; gap: 30;">
                                                 <p class="new_p">Invoice Date<span style="margin-left: 60px;">:{{ $invoice_date }}</span></p>
-                                                <p class="new_p" style="margin-top:16px;">Invoice No.<span style="margin-left: 64px; margin-top: 16px">:#{{ $invoice_number }}</p>
+                                                <p class="new_p" style="margin-top:8px;">Invoice No.<span style="margin-left: 64px; margin-top: 16px">:#{{ $invoice_number }}</p>
                                             </div>     
                                         </div>
                                     </td>
