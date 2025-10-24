fff77105d51e3efc29ecc9259edf508a9cf427df
saakshichat <saakshi@supporthives.com>
2025-10-24 13:28:43 +0530

    applying final change

--- resources/views/websites/ecommerce/twohundredthirty-eight.blade.php
+++ resources/views/websites/ecommerce/twohundredthirty-eight.blade.php
@@ -77,7 +77,7 @@
 
                     <!-- Content -->
                     <tr>
-                        <td style="padding:20px;padding-bottom: 0px;width:80%;vertical-align:top;" align="center">
+                        <td style="padding:20px;padding-bottom: 0px;width:80%;vertical-align:top;margin-top:10px;" align="center">
                             <table cellspacing="0" cellpadding="0" border="0" width="100%"
                                 style="border-collapse: collapse;margin-top:40px;">
                                 <tr style="height:50px;border-bottom:1px solid black;">
