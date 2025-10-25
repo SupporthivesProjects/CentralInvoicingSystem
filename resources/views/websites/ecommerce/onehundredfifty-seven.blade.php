65372bee569b0603a4cd7da6a4e8d074e18d4c8e
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-10-25 14:39:51 +0530

    change

--- resources/views/websites/ecommerce/onehundredfifty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredfifty-seven.blade.php
@@ -41,8 +41,8 @@
                     <!-- BILL FROM / BILL TO -->
                     <tr>
                         <td colspan="3" style="padding: 10px; background-color: #f1e9df; font-weight: bold">BILL FROM</td>
-                        <td colspan="1" style="padding: 10px; background-color: #ffffff; font-weight: bold ; min-width: 30px"></td>
-                        <td colspan="1" style="padding: 10px; background-color: #f1e9df; font-weight: bold">BILL TO</td>
+                        <td colspan="1" style="padding: 10px; background-color: #ffffff; font-weight: bold ; max-width: 40px"></td>
+                        <td colspan="2" style="padding: 10px; background-color: #f1e9df; font-weight: bold">BILL TO</td>
                     </tr>
                     <tr>
                         <td colspan="3" style="padding: 10px; border-bottom: 1px solid #ddd; font-size: 14px">
@@ -51,8 +51,8 @@
                             <strong>Phone :</strong> {{ $company_mobile }}<br />
                             <strong>Email :</strong> {{ $company_email }}
                         </td>
-                        <td colspan="1" style="padding: 10px; border-bottom: 1px solid #ddd; font-size: 14px; min-width: 30px"></td>
-                        <td colspan="1" style="padding: 10px; border-bottom: 1px solid #ddd;">
+                        <td colspan="1" style="padding: 10px; border-bottom: 1px solid #ddd; font-size: 14px; max-width: 40px"></td>
+                        <td colspan="2" style="padding: 10px; border-bottom: 1px solid #ddd;">
                             <p style="margin-top: -50px; font-size: 14px;"><strong>{{ $customer_name }}</strong></p><br />
                             <!-- <strong>Phone :</strong> {{ $customer_mobile }}<br />
                             <strong>Email :</strong> {{ $customer_email }} -->
