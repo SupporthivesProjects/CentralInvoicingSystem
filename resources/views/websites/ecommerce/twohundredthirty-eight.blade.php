94ad14c6e8933e1140e224843947483f0642b89a
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-10-29 11:27:39 +0530

    238 3

--- resources/views/websites/ecommerce/twohundredthirty-eight.blade.php
+++ resources/views/websites/ecommerce/twohundredthirty-eight.blade.php
@@ -146,7 +146,7 @@
                                         <p
                                             style="font-size:9px;font-weight:500;font-family:Urbanist;margin: 0px;line-height: 28px;text-align:center;padding-right:10px;text-transform: uppercase;">
                                             {{ site_currency_code() . number_format($invoice_amount + $discount_amount, 2) }}
-
+                                            {{ site_currency() }} {{ number_format($product->unit_price ?? 0, 2) }}
                                         </p>
                                     </td>
                                 </tr>
