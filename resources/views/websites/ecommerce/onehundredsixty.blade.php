50eeda9eadfc5dff4045bffee9386e1fe5d3da50
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-10-29 12:37:50 +0530

    160 2

--- resources/views/websites/ecommerce/onehundredsixty.blade.php
+++ resources/views/websites/ecommerce/onehundredsixty.blade.php
@@ -41,7 +41,7 @@
                                                 background-position: top center;
                                                 background-size: cover;
                                                 width: calc(100% + 5px);
-                                                padding-left: 120px;
+                                                padding-left: 140px;
                                                 display: flex;
                                                 flex-direction: column;
                                                 justify-content: center;
