06056f3e2625fe527a70e73f959d5d6180141059
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2025-10-29 12:00:29 +0530

    gp issues

--- resources/views/websites/ecommerce/ninety-one.blade.php
+++ resources/views/websites/ecommerce/ninety-one.blade.php
@@ -92,7 +92,7 @@
                                             </td>
                                         </tr>
                                         @foreach($products as $product)
-                                        <tr style="border-top: 1px solid black;border-bottom: 1px solid black;height:50px;">
+                                        <tr style="border-top: 1px solid black;border-bottom: 1px solid black;height:40px;">
                                             <td style="width: 50%;padding-left: 10px;">
                                                  <p style="margin: 0px;font-family:Source Sans Pro;font-size:9px;color: #000000;">
                                                   {{ $product->name }}
