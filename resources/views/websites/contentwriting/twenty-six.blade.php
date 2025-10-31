c57de4e0a8ae6d203f7cbaf92b711d7bb019421b
KadamSaakshi <sakshi.k@supporthives.com>
2025-10-31 22:52:47 +0530

    twenty six

--- resources/views/websites/contentwriting/twenty-six.blade.php
+++ resources/views/websites/contentwriting/twenty-six.blade.php
@@ -114,7 +114,7 @@
                                             <img src="{{ $invoice_image2 }}" alt="" style="width: 24px;">
                                             <div class="linement2">
                                                 <p style="color: #136476; font-weight: 700;">Website Address:</p>
-                                                    {{ $site->site_link }}
+                                                    <a href="{{ $site->site_link }}">www.kontikilab.com</a>
                                             </div>
                                         </div>
                                     </td>
