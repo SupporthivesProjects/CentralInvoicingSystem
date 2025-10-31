0b5cc6a6ac61eb3e79f031d5d6f4a2c481cae673
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-10-31 14:41:13 +0530

    brandexx

--- resources/views/websites/marketing/onehundredsixty-four.blade.php
+++ resources/views/websites/marketing/onehundredsixty-four.blade.php
@@ -45,16 +45,16 @@
             <td colspan="2" style="padding:0 30px 0 260px; ">
                 <table align="top" valign="top" width="100%" cellpadding="0" cellspacing="0" style="margin-top:100px;position: relative; top: -60px;">
                     <tr>
-                        <td style="font-size:0.98em; padding-right:20px;width:32%;vertical-align: top;">
-                        <div style="display:flex; flex-direction:row; justify-content:flex-start; align-items:center; gap: 4px;"><img src="{{ $invoice_image3 }}" alt="Company Logo" width="25" height="25"><b>Phone:</b><br></div>
+                        <td style="font-size:0.98em; padding-right:17px;width:33%;vertical-align: top;">
+                        <div style="display:flex; flex-direction:row; justify-content:flex-start; align-items:center; gap: 4px;margin-bottom: 4px;"><img src="{{ $invoice_image3 }}" alt="Company Logo" width="25" height="25"><b>Phone:</b><br></div>
                             {{ $company_mobile }}
                         </td>
-                        <td style="font-size:0.98em; padding-right:20px;width:28%;vertical-align: top;">
-                        <div style="display:flex; flex-direction:row; justify-content:flex-start; align-items:center; gap: 4px;"><img src="{{ $invoice_image2 }}" alt="Company Logo" width="25" height="25"><b>Email:</b><br></div>
+                        <td style="font-size:0.98em; padding-right:20px;width:27%;vertical-align: top;">
+                        <div style="display:flex; flex-direction:row; justify-content:flex-start; align-items:center; gap: 4px;margin-bottom: 4px;"><img src="{{ $invoice_image2 }}" alt="Company Logo" width="25" height="25"><b>Email:</b><br></div>
                             {{ $company_email }}
                         </td>
                         <td style="font-size:0.98em;">
-                        <div style="display:flex; flex-direction:row; justify-content:flex-start; align-items:center; gap: 4px;"><img src="{{ $invoice_image1 }}" alt="Company Logo" width="25" height="25"><b>Address:</b><br></div>
+                        <div style="display:flex; flex-direction:row; justify-content:flex-start; align-items:center; gap: 4px;margin-bottom: 4px;"><img src="{{ $invoice_image1 }}" alt="Company Logo" width="25" height="25"><b>Address:</b><br></div>
                             {!! $company_address !!}
                         </td>
                     </tr>
