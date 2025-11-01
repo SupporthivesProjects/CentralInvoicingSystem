f2906a57a7206ef674b1c5da22616b60c5eb2f8e
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2025-11-01 12:29:27 +0530

    solving brandsparkz issues

--- resources/views/websites/marketing/twohundredthirty-four.blade.php
+++ resources/views/websites/marketing/twohundredthirty-four.blade.php
@@ -159,7 +159,7 @@
                                 <tr style="height: 100px; font-family: 'Poppins', sans-serif;">
 
                                     <!-- Column 1 -->
-                                    <td style="vertical-align: top;width: 25%; text-align: left; font-size: 10px; color: #5E5E5E; padding-left: 40px; padding-top: 10px;">
+                                    <td style="vertical-align: top;width: 20%; text-align: left; font-size: 10px; color: #5E5E5E; padding-left: 40px; padding-top: 10px;">
                                         @if(!empty($company_email))
                                         <div style="display: flex; align-items: center;">
                                             <img src="{{ $invoice_image2 }}" alt="" style="height: 25px; vertical-align: middle;">
@@ -170,7 +170,7 @@
                                     </td>
 
                                     <!-- Column 2 -->
-                                    <td style="vertical-align: top;width: 25%; text-align: left; font-size: 10px; color: #5E5E5E; padding-top: 10px;">
+                                    <td style="vertical-align: top;width: 30%; text-align: left; font-size: 10px; color: #5E5E5E; padding-top: 10px;">
                                         @if(!empty($company_mobile))
                                         <div style="display: flex; align-items: center;">
                                             <img src="{{ $invoice_image3 }}" alt="" style="height: 25px; vertical-align: middle;">
