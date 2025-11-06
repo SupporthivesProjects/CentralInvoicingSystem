1326e132e9f1511dd3046ee9cdb39b3b9fb42753
saakshichat <saakshi@supporthives.com>
2025-11-06 14:21:06 +0530

    bold text

--- resources/views/websites/ecommerce/twenty-five.blade.php
+++ resources/views/websites/ecommerce/twenty-five.blade.php
@@ -135,13 +135,13 @@
                                         <td style="width: 100px;text-align: left;font-family: Courier Newl;font-size: 10px;margin: 0px;font-weight: 700; border-collapse: collapse;">
                                         
                                         </td>
-                                        <td style="width: 300px;text-align:left;padding-left:10px;font-family: arial;font-size:10px;margin: 0px;font-weight: 700; border-collapse: collapse;background-color: #FF4500; color: white;">
+                                        <td style="width: 300px;text-align:left;padding-left:10px;font-family: arial;font-size:10px;margin: 0px;font-weight:900; border-collapse: collapse;background-color: #FF4500; color: white;">
                                           <b>GRAND TOTAL</b>  
                                         </td>
                                         <td style="width:100px;text-align:left;padding-right:10px;font-family: Courier New;font-size: 10px;margin: 0px;font-weight: 700; border-collapse: collapse;background-color: #FF4500;">
                                         
                                         </td>
-                                        <td style="width:100px;text-align:left;padding-right:10px;font-family: Courier New;font-size: 10px;margin: 0px;font-weight: 700; border-collapse: collapse;background-color: #FF4500; color: white;">
+                                        <td style="width:100px;text-align:left;padding-right:10px;font-family: Courier New;font-size: 10px;margin: 0px;font-weight:900; border-collapse: collapse;background-color: #FF4500; color: white;">
                                           <b> {{ site_currency() . number_format($invoice_amount, 2) }}</b> 
                                         </td>
                                     </tr><br><br>
