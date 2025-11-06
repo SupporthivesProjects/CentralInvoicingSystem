8fdd1b691dbea041ede646489f8f72c318c27387
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-11-06 06:32:23 +0530

    The Web Design Crowd

--- resources/views/websites/ecommerce/eighty-two.blade.php
+++ resources/views/websites/ecommerce/eighty-two.blade.php
@@ -110,10 +110,10 @@
                     <!---header End---->
                     <!-- Content -->
                     <tr style="background:#ffff ;">
-                        <td style="padding: 40px 20px;padding-bottom: 0px;min-height: 900px;">
-                            <table border="0" style="border-collapse: collapse;width: 100%;min-height: 900px;">
+                        <td style="padding: 40px 20px;padding-bottom: 0px;min-height: 950px;">
+                            <table border="0" style="border-collapse: collapse;width: 100%;min-height: 950px;">
                                 <tr>
-                                    <td style="min-height: 900px;width: 30%;background: url('{{ $invoice_image3 }}');background-repeat:no-repeat;background-size: cover;padding: 20px 20px 20px 10px;vertical-align: top;"
+                                    <td style="min-height: 950px;width: 30%;background: url('{{ $invoice_image3 }}');background-repeat:no-repeat;background-size: cover;padding: 20px 20px 20px 10px;vertical-align: top;"
                                         align="center">
                                         <img src="{{ $company_logo }}" alt="" style="width:40px">
                                         <br>
