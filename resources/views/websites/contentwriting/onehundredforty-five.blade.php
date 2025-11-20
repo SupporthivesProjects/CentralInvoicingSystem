f8499ad26c084e209f3056d0d8dfeb89103636fc
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-11-20 15:32:21 +0530

    change

--- resources/views/websites/contentwriting/onehundredforty-five.blade.php
+++ resources/views/websites/contentwriting/onehundredforty-five.blade.php
@@ -151,7 +151,7 @@
                     </tr>
                     <tr>
                         <td style="padding:0px 56px 50px">
-                            <div style="height: 615px;">
+                            <div style="height: 612px;">
                                 <table class="table-list" style="width: 100%;"> 
                                     <tbody>
                                         <tr>
