ed98df10ac12584b498d29237b361a1a856e976c
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-11-28 12:55:33 +0530

    thewebzter

--- resources/views/websites/ecommerce/onehundredforty-eight.blade.php
+++ resources/views/websites/ecommerce/onehundredforty-eight.blade.php
@@ -182,13 +182,13 @@
                                                 <div class="box">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#D2B76A" d="M290.5 287.7L491.4 86.9 359 456.3 290.5 287.7zM457.4 53L256.6 253.8 88 185.3 457.4 53zM38.1 216.8l205.8 83.6 83.6 205.8c5.3 13.1 18.1 21.7 32.3 21.7 14.7 0 27.8-9.2 32.8-23.1L570.6 8c3.5-9.8 1-20.6-6.3-28s-18.2-9.8-28-6.3L39.4 151.7c-13.9 5-23.1 18.1-23.1 32.8 0 14.2 8.6 27 21.7 32.3z"/></svg>
                                                 </div>
-                                                <a href="{{ $company_email }}" style="margin-bottom: 0px;margin-top:5px; text-decoration: none;"></a>
+                                                <h6 style="margin-bottom: 0px;margin-top:5px;">{{ $company_email }}</h6>
                                             </div>
                                            <div class="linement">
                                                 <div class="box">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Pro v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2025 Fonticons, Inc.--><path fill="#D2B76A" d="M351.9 280l-190.9 0c2.9 64.5 17.2 123.9 37.5 167.4 11.4 24.5 23.7 41.8 35.1 52.4 11.2 10.5 18.9 12.2 22.9 12.2s11.7-1.7 22.9-12.2c11.4-10.6 23.7-28 35.1-52.4 20.3-43.5 34.6-102.9 37.5-167.4zM160.9 232l190.9 0C349 167.5 334.7 108.1 314.4 64.6 303 40.2 290.7 22.8 279.3 12.2 268.1 1.7 260.4 0 256.4 0s-11.7 1.7-22.9 12.2c-11.4 10.6-23.7 28-35.1 52.4-20.3 43.5-34.6 102.9-37.5 167.4zm-48 0C116.4 146.4 138.5 66.9 170.8 14.7 78.7 47.3 10.9 131.2 1.5 232l111.4 0zM1.5 280c9.4 100.8 77.2 184.7 169.3 217.3-32.3-52.2-54.4-131.7-57.9-217.3L1.5 280zm398.4 0c-3.5 85.6-25.6 165.1-57.9 217.3 92.1-32.7 159.9-116.5 169.3-217.3l-111.4 0zm111.4-48C501.9 131.2 434.1 47.3 342 14.7 374.3 66.9 396.4 146.4 399.9 232l111.4 0z"/></svg>
                                                 </div>
-                                                <h6 style="margin-bottom: 0px;margin-top:5px;">{{ $site->site_link }}</h6>
+                                                <a href="{{ $site->site_link }}" style="margin-bottom: 0px;margin-top:5px; text-decoration: none;">www.thewebzter.com </a>
                                            </div>
                                            <div class="linement">
                                                 <div class="box">
