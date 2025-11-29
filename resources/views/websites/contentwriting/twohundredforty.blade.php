fe17184f25170278b844bc02b04a474b4f4c0c28
tirthak <gadgetzone@tirthak-MacBook-Pro.local>
2025-11-29 13:23:39 +0530

    erwds

--- resources/views/websites/contentwriting/twohundredforty.blade.php
+++ resources/views/websites/contentwriting/twohundredforty.blade.php
@@ -85,7 +85,7 @@
                                     </td>
                                 </tr>
                                 @foreach ($products as $product)
-                                <tr style="border-collapse: collapse;height: 30px;font-family: 'Sen', sans-serif; color: gray;border-bottom: 1px solid black;background-color: {{ $loop->iteration % 2 == 0 ? '#f2f2f2' : '#ffffff' }}; ">
+                                <tr style="border-collapse: collapse;height: 30px;font-family: 'Sen', sans-serif; color: gray;border-bottom: 1px solid black;background-color: {{ $loop->iteration % 2 == 0 ? lightgrey : '#ffffff' }}; ">
                                     <td style="width: 50px;text-align: left;font-size: 14px;margin: 0px;font-weight: 400;border-collapse: collapse;padding-left: 5px;">
                                        {{ $loop->iteration }}
                                     </td>
@@ -191,11 +191,18 @@
                                             <td style="width: 35%"> 
                                             <img src="{{ $invoice_image1 }}" style="height: 200px;">
                                             </td>
-                                            <td style="text-align: center;font-size: 14px;display: flow-root; font-family: 'Sen', sans-serif; width: 80%; ">
-                                                <p><b style="color: #172355;">TELs:</b> {{ $company_mobile }}</p>
+                                            <!-- <td style="text-align: center;font-size: 14px;display: flow-root; font-family: 'Sen', sans-serif; width: 80%; ">
+                                                <p><b style="color: #172355;">TEL:</b> {{ $company_mobile }}</p>
                                                 <p><b style="color: #172355;">EMAIL:</b> {{ $company_email }}</p>
                                                 <p><b style="color: #172355;">ADDRESS:</b> {!! $company_address !!}</p>
-                                            </td > 
+                                            </td >  -->
+                                            <td style="text-align:center; font-size:14px; font-family:'Sen', sans-serif; width:80%;">
+                                                <div style="margin-left:-20px;">
+                                                    <p><b style="color:#172355;">TEL:</b> {{ $company_mobile }}</p>
+                                                    <p><b style="color:#172355;">EMAIL:</b> {{ $company_email }}</p>
+                                                    <p><b style="color:#172355;">ADDRESS:</b> {!! $company_address !!}</p>
+                                                </div>
+                                            </td>
                                             <td style="width: 35%">
                                                 <img src="{{ $invoice_image2 }}" style="height: 200px;">
                                             </td>   
