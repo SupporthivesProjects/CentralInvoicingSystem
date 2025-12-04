6d79f10daceeb6075fc0c0cf8cfbb2c7d5d4495a
KadamSaakshi <sakshi.k@supporthives.com>
2025-12-04 18:27:45 +0530

    two hundred fourty two

--- resources/views/websites/marketing/twohundredforty-two.blade.php
+++ resources/views/websites/marketing/twohundredforty-two.blade.php
@@ -13,7 +13,7 @@
                     style="border: 15px solid #D95D27;background: #D95D27;">
                     <tr>
                         <td>
-                            <table width="100%" cellspacing="0" cellpadding="0" border="0" style="border-collapse: collapse;border-radius: 18px;background-color: #ffffff; height: 1000px;">
+                            <table width="100%" cellspacing="0" cellpadding="0" border="0" style="border-collapse: collapse;border-radius: 18px;background-color: #ffffff; height: 980px;">
                                 <!-- Header -->
                                 <tr>
                                     <td style="height: 165px;">
@@ -29,7 +29,7 @@
                                 <!-- Content -->
                                 <tr>
 
-                                    <td style="font-family: 'Montserrat'; font-size: 9px; vertical-align: top; height: 320px; background-color:red; ">
+                                    <td style="font-family: 'Montserrat'; font-size: 9px; vertical-align: top; height: 320px;">
 
                                         <table width="100%" cellpadding="0" style="padding:40px 80px; padding-bottom: 0%; ">
                                             <tr>
