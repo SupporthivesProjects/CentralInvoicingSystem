8fc239e2287a44ee220eaaa780bada188f5bc695
saakshichat <saakshi@supporthives.com>
2025-12-05 14:00:04 +0530

    trying new fixes

--- resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
@@ -21,8 +21,8 @@
                     <tr>
                         <td>
                             <table border="0" style="border-collapse:collapse;padding:0" width="100%">
-                                <tr style="background: url('{{ $invoice_image3 }}');background-position: center;background-repeat: no-repeat;background-size: cover;height:200px;">
-                                    <td style="padding:0px 100px" align="right" style="vertical-align:ceter">
+                                <tr style="background: url('{{ $invoice_image3 }}');background-position: center;background-repeat: no-repeat;background-size: cover;height:170px;">
+                                    <td style="padding:0px" align="right" style="vertical-align:ceter">
                                     <div style="width:40%;padding-left:50px;text-align: left;">
                                         <p style="margin: 0px;font-size: 10px;font-family: Lato;color: #90b3eb;line-height: 16px;">
                                         Invoice Number
