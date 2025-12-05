844c1a17343f9c7e67a4bb4357372043c104b935
KadamSaakshi <sakshi.k@supporthives.com>
2025-12-05 16:40:29 +0530

    one hundred nine

--- resources/views/websites/ecommerce/onehundrednine.blade.php
+++ resources/views/websites/ecommerce/onehundrednine.blade.php
@@ -76,7 +76,7 @@
                                         </p>
 
                                         <p style="margin: 0px;font-family: Arial;font-size: 10px;width: 200px; line-height: 18px;">
-                                            <b>Address : </b>{!! $company_address !!}
+                                            <b>Address : sahyiedj jcjdschd ksjadjid ccnvbdhhsn 123 vk mjdlxc kcslcjm skcjjc</b>{!! $company_address !!}
                                         </p>
                                     </td>
                                 </tr>
