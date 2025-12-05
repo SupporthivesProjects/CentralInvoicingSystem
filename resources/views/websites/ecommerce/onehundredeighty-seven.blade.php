c6dc16693bd03a93cee0cec0ced16885b870d3b7
saakshichat <saakshi@supporthives.com>
2025-12-05 14:08:22 +0530

    header

--- resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
@@ -20,8 +20,8 @@
                    <!-- header -->
                     <tr>
                         <td>
-                            <table border="0" style="border-collapse:collapse;padding:0" width="100%">
-                                <tr style="background: url('{{ $invoice_image3 }}');background-position: center;background-repeat: no-repeat;background-size: cover;height:100%;">
+                            <table border="0" style="border-collapse:collapse;padding:0;height:150px;" width="100%">
+                                <tr style="background: url('{{ $invoice_image3 }}');background-position: center;background-repeat: no-repeat;background-size:100% 100%;height:150px;">
                                     <td style="padding:0px" align="right" style="vertical-align:ceter">
                                     <div style="width:40%;padding-left:50px;text-align: left;">
                                         <p style="margin: 0px;font-size: 10px;font-family: Lato;color: #90b3eb;line-height: 16px;">
