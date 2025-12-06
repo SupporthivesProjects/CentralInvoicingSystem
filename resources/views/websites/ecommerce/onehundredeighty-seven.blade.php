c636041b7b1cd25ff9530f62bb4203b67c49698a
saakshichat <saakshi@supporthives.com>
2025-12-06 16:37:03 +0530

    bottom fix new

--- resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
@@ -77,7 +77,7 @@
                     <!-- some data -->
 
                       <!-- content -->
-                        <tr style="height:829px">
+                        <tr style="height:835px">
                             <td style="vertical-align: top;padding: 0px 50px;">
                              
 
