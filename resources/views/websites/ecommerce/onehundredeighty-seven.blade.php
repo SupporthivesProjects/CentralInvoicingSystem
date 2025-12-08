bec8e2a4347e9797852679ec013dbb0e7ddb9561
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-12-08 14:57:09 +0530

    sitedesign made easy

--- resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
@@ -14,8 +14,9 @@
         }
     </style>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
@@ -194,7 +195,7 @@
                     <!-- content -->
                 </table>
                 <div style="rotate: 90deg;position: absolute; top: 500px; right: -50px; border: 1px solid red;">
-                    <h1 style="color: #FFFFFF;font-size: 50px;letter-spacing: 20px;font-family: Lato;">INVOICE</h1>
+                    <h1 style="color: #FFFFFF;font-size: 50px;letter-spacing: 20px;font-family: Montserrat;">INVOICE</h1>
                 </div>
             </td>
         </tr>
