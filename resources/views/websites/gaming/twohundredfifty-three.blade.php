d63fcdca8d57c629e6b1cf91ea8961e04c6b84d8
Rohit <rohit@supporthives.com>
2025-12-08 11:30:03 +0530

    code

---
+++ resources/views/websites/gaming/twohundredfifty-three.blade.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Email Title</title>
+</head>
+<style>
+    table tr:nth-last-child(4) {
+    border-bottom: 1px solid #FFFFFF;
+}
+</style>
+<body style="padding: 0px; margin: 0px;">
+    <table width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tr>
+            <td align="center" bgcolor="#f2f2f2" style="padding: 0px;">
+                <table width="700" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse; box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);">
+                
+                    <!-- Content -->
+                    <tr>
+                        <td style="padding: 95px 120px 170px 40px;background: url(./img/body_bg.jpg);background-position: center;background-size: cover;height:444px;">
+                            <table style="width:100%;">
+                                <tr>
+                                    <td>
+                                        <p style="line-height: 165%; font-family: Arial;font-size: 48px;margin: 0px;font-weight: 400; color: #FFFFFF;">
+                                           INVOICE
+                                        </p>
+                                        <br>
+                                        <div style="display: flex; justify-content: space-between; width: 100%;">
+                                            <p style="line-height: 165%; width: 50%; font-family: Arial;font-size: 11px;margin: 0px;font-weight: 400; color: #FFFFFF;">
+                                                Invoice To :<br>
+                                                <span style="font-size: 14px;"><b>Geoffrey K. Haugland</b></span>
+                                            </p>
+                                            <p style="line-height: 165%; width: 50%; padding-left: 100px; font-family: Arial;font-size: 11px;margin: 0px;font-weight: 400; color: #FFFFFF;">
+                                                <span style="color: #ec4185;"><b>Invoice No.</b></span><br>
+                                                1234-5678<br>
+                                                <span style="color: #ec4185; margin-top: 16px;"><b>Invoice No.</b></span><br>
+                                                03/25/2025<br>
+                                            </p>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                            <br>
+                            <br>
+                            <br>
+                            <diV style="height: 500px; padding-left: 20px;">
+                                <table style="border-collapse: collapse;">
+                                    <tr style="border-collapse: collapse;height: 24px;">
+                                        <td style="width: 250px; color: #FFFFFF; text-align: start; padding: 20px 10px;font-family:  Arial;font-size: 14px;margin: 0px;font-weight: 400;border-bottom: 1px solid #FFFFFF;border-collapse: collapse;">
+                                           <b>DESCRIPTION</b> 
+                                        </td>
+                                        <td style="width: 100px; color: #FFFFFF; text-align: center; padding: 20px 10px;font-family:  Arial;font-size: 14px;margin: 0px;font-weight: 400;border-bottom: 1px solid #FFFFFF;border-collapse: collapse;">
+                                            <b>UNIT&nbsp;PRICE</b>
+                                        </td>
+                                        <td style="width: 150px; color: #FFFFFF; text-align: center; padding: 20px 10px; font-family:  Arial;font-size: 14px;margin: 0px;font-weight: 400;border-bottom: 1px solid #FFFFFF;border-collapse: collapse;">
+                                            <b>QTY</b>
+                                        </td>
+                                        <td style="width: 150px; color: #FFFFFF; text-align: end; padding: 20px 10px; font-family:  Arial;font-size: 14px;margin: 0px;font-weight: 400;border-bottom: 1px solid #FFFFFF;border-collapse: collapse;">
+                                            <b>TOTAL</b>
+                                        </td>
+                                    </tr>
+                                    <tr style="border-collapse: collapse;height: 24px;">
+                                        <td style="width: 250px; color:#FFFFFF; text-align: start; padding: 16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            <b>Item Name</b>
+                                        </td>
+                                        <td style="width: 100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size:12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $100.00
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            2
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:right;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $200.00
+                                        </td>
+                                    </tr>
+                                    <tr style="border-collapse: collapse;height: 24px;">
+                                        <td style="width: 250px; color:#FFFFFF; text-align: start; padding: 16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            <b>Item Name</b>
+                                        </td>
+                                        <td style="width: 100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size:12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $100.00
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            2
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:right;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $200.00
+                                        </td>
+                                    </tr>
+                                    <tr style="border-collapse: collapse;height: 24px;">
+                                        <td style="width: 250px; color:#FFFFFF; text-align: start; padding: 16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            <b>Item Name</b>
+                                        </td>
+                                        <td style="width: 100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size:12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $100.00
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            2
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:right;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $200.00
+                                        </td>
+                                    </tr>
+                                    <tr style="border-collapse: collapse;height: 24px;">
+                                        <td style="width: 250px; color:#FFFFFF; text-align: start; padding: 16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            <b>Item Name</b>
+                                        </td>
+                                        <td style="width: 100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size:12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $100.00
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            2
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:right;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $200.00
+                                        </td>
+                                    </tr>
+                                    <tr style="border-collapse: collapse;height: 24px;">
+                                        <td style="width: 250px; color:#FFFFFF; text-align: start; padding: 16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            <b>Item Name</b>
+                                        </td>
+                                        <td style="width: 100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size:12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $100.00
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:center;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            2
+                                        </td>
+                                        <td style="width:100px; color:#FFFFFF; text-align:right;padding:16px 10px;font-family:  Arial;font-size: 12px;margin: 0px;font-weight: 400;border-collapse: collapse;">
+                                            $200.00
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="    text-align: start; font-family: Arial; font-size: 10px; margin: 0px; font-weight: 400; padding-left: 40px; color: #FFFFFF; font-style: italic; padding-top: 50px;" colspan="2">
+                                            Company Name<br>
+                                            Company Address
+                                           </td>
+                                        <td style="width: 100px;color: #FFFFFF;text-align: start;font-family: Arial;font-size: 12px;margin: 0px;font-weight: 400; padding-top: 40px;" colspan="1">
+                                         <p>SUBTOTAL</p>
+                                        </td>
+                                        <td style="width:100px;color: #FFFFFF;text-align:end;padding-right:10px;font-family: Arial;font-size: 12px;margin: 0px;font-weight: 400; padding: 0px 10px;border-collapse: collapse; padding-top: 40px;">
+                                            <p>$720</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 100px;text-align: right;font-family: Arial;font-size: 10px;margin: 0px;font-weight: 400;padding-right: 10px; " colspan="2">
+                                           </td>
+                                        <td style="width: 100px;color: #FFFFFF;text-align: start;font-family: Arial;font-size: 12px;margin: 0px;font-weight: 400; padding: 0px 10px;"  colspan="1">
+                                         <p>
+                                            DISCOUNT
+                                        </p>
+                                        </td>
+                                        <td style="width:100px;color: #FFFFFF;text-align:end;padding-right:10px;font-family: Arial;font-size: 12px;margin: 0px;font-weight: 400; padding: 0px 10px;border-collapse: collapse;">
+                                            <p>$0.00</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 100px;text-align: right;font-family: Arial;font-size: 10px;margin: 0px;font-weight: 400;padding-right: 10px;" colspan="2">
+                                           </td>
+                                           <td style="width: 100px;color: #FFFFFF;text-align: start;font-family: Arial;font-size: 12px;margin: 0px;font-weight: 400; padding: 0px 10px; background-color: #ec4185;"  colspan="1">
+                                            <p>
+                                               TOTAL
+                                           </p>
+                                           </td>
+                                           <td style="width:100px;color: #FFFFFF;text-align:end;padding-right:10px;font-family: Arial;font-size: 12px;margin: 0px;font-weight: 400; padding: 0px 10px; background-color: #ec4185;border-collapse: collapse;">
+                                               <p>$792</p>
+                                           </td>
+                                    </tr>
+                                </table>
+                            </diV>
+                            
+                        </td>
+                    </tr>
+                    <!-- Content End-->
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
