e50b7d713954bce7e21be46e9fa2dfa20f259c47
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-12-08 15:08:47 +0530

    sitedesign made easy

--- resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
@@ -195,7 +195,7 @@
                     <!-- content -->
                 </table>
                 <div style="rotate: 90deg;position: absolute; top: 500px; left: 69%;">
-                    <h1 style="color: #FFFFFF;font-size: 80px;letter-spacing: 15px;font-family: Montserrat;">INVOICE</h1>
+                    <h1 style="color: #FFFFFF;font-size: 70px;letter-spacing: 15px;font-family: Montserrat;">INVOICE</h1>
                 </div>
             </td>
         </tr>
