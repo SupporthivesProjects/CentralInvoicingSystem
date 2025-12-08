55dc55c253042d8b2134d81ed396d87ac9158139
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-12-08 15:25:12 +0530

    sitedesign made easy

--- resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
@@ -18,7 +18,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
-<body style="background: url('{{ $invoice_image4 }}');background-position: top;background-repeat: no-repeat;background-size:cover;height:1000px;">
+<body style="background: url('{{ $invoice_image4 }}');background-position: top;background-repeat: no-repeat;background-size:cover;height:950px;">
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
         <tr>
             <td align="center" style="padding:0px;">
