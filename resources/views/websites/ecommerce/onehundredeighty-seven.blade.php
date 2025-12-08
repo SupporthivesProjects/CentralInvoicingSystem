b2f64979e8ae7b6a0d5e9a078bddc33cfe18e173
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-12-08 15:12:04 +0530

    sitedesign made easy

--- resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
@@ -22,7 +22,7 @@
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
         <tr>
             <td align="center" style="padding:0px;">
-                <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse;background: url('{{ $invoice_image4 }}');background-position: top;background-repeat: no-repeat;background-size:cover;height:1120px;">
+                <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse;background: url('{{ $invoice_image4 }}');background-position: top;background-repeat: no-repeat;background-size:cover;height:1050px;">
                    <!-- header -->
                     <tr style="height: 150px;">
                         <td colspan="2" style="vertical-align:top;padding-top:90px;padding-bottom:90px">
@@ -194,7 +194,7 @@
                         </tr>
                     <!-- content -->
                 </table>
-                <div style="rotate: 90deg;position: absolute; top: 500px; left: 69%;">
+                <div style="rotate: 90deg;position: absolute; top: 550px; left: 60%;">
                     <h1 style="color: #FFFFFF;font-size: 70px;letter-spacing: 15px;font-family: Montserrat;">INVOICE</h1>
                 </div>
             </td>
