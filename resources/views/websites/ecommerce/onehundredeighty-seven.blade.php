c4a7e5e455703f8a11c2f16db8de002e4edfb1f1
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-12-08 15:18:48 +0530

    sitedesign made easy

--- resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
+++ resources/views/websites/ecommerce/onehundredeighty-seven.blade.php
@@ -22,7 +22,7 @@
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
         <tr>
             <td align="center" style="padding:0px;">
-                <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" style="border-collapse: collapse;">
+                <table width="100%" cellspacing="0" cellpadding="0" border="0" style="border-collapse: collapse;">
                    <!-- header -->
                     <tr style="height: 150px;">
                         <td colspan="2" style="vertical-align:top;padding-top:90px;padding-bottom:90px">
