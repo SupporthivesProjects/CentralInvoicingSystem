1555e2fce4ad443ac6a7287f501d7aca1f261e37
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-12-13 17:21:47 +0530

    change

--- resources/views/websites/ecommerce/ninety-two.blade.php
+++ resources/views/websites/ecommerce/ninety-two.blade.php
@@ -60,7 +60,7 @@
                                   </td>
                                 </tr>
                               </table>
-                          <div style="min-height: 540px !important;">
+                          <div style="min-height: 542px !important;">
                             <table style="width: 100%; border-collapse: collapse; font-family: Arial, sans-serif; font-size: 8px;">
                                 <tr style="background-color: #A9BF2B;">
                                   <th style="text-align: left; padding: 8px 20px; font-size: 12px;">DESCRIPTION</th>
