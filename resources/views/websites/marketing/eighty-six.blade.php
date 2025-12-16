436d60953c9f5483eeae2760ab4a289791e5f9d2
Rahul Yadav <supportshives@gadgetzones-MacBook-Air.local>
2025-12-16 14:03:29 +0530

    change

--- resources/views/websites/marketing/eighty-six.blade.php
+++ resources/views/websites/marketing/eighty-six.blade.php
@@ -5,7 +5,7 @@
     <title>{{ $site_name }} - Invoice #{{ $invoice_number }}</title>
 </head>
 
-<body>
+<body style="margin: 0px; padding: 0px;">
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
         <tr>
             <td align="center" bgcolor="#f2f2f2" style="padding: 0px;">
