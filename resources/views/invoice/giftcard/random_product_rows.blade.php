3e860aad85dc443f5ee8470280f944885618f02e
Mr. Narayan <narayan.d.zade@gmail.com>
2025-12-23 16:53:06 +0530

    working on card business model conversion price updates

--- resources/views/invoice/giftcard/random_product_rows.blade.php
+++ resources/views/invoice/giftcard/random_product_rows.blade.php
@@ -53,7 +53,7 @@
             <span class="input-group-text"  data-bs-toggle="tooltip" title="{{ site_currency_code() }}">{{ site_currency() }}</span>
             <input type="text" 
                    class="form-control text-center product-rrp" 
-                   value="{{ number_format($product->rrp, 0, '.', '') }}" 
+                   value="{{ number_format($product->rrp, 2) }}" 
                    data-reverse-rate="{{ $product->reverse_rate ?? 1 }}"
                    data-product-id="{{ $product->id }}"
                    aria-label="RRP"
