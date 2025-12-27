174e9f2705ccaf5116e4725abd058e5caaea3595
tirthak <gadgetzone@tirthak-MacBook-Pro.local>
2025-12-27 14:06:09 +0530

    erwfd

--- resources/views/websites/gaming/twohundredfifty.blade.php
+++ resources/views/websites/gaming/twohundredfifty.blade.php
@@ -7,21 +7,20 @@
     <style>
         @page {
             margin: 0;
-            size: A4;
+            size: 210mm 297mm;
         }
 
         html, body {
-            width: 100%;
-            height: 100%;
+            width: 210mm;
+            height: 297mm;
             margin: 0;
             padding: 0;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-        body {
-            background: url({{$invoice_image1}}) no-repeat center center;
-            background-size: cover;
         }
 
+        body {
+            background: url({{$invoice_image1}}) no-repeat center center;
+            background-size: 210mm 297mm;
+        }
         h1,h2,h3,h4,h5,h6,p {
             margin: 0;
         }
