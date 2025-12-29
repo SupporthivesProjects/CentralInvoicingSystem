c27b6cb6b0b8c47441111f056f512e4e6455a520
Dev Rishav <coder.rishav@gmail.com>
2025-12-29 12:22:54 +0530

    Content sites Word Cound Increase Issue Fixed

--- resources/views/invoice/contentwriting/productSelection.blade.php
+++ resources/views/invoice/contentwriting/productSelection.blade.php
@@ -1289,7 +1289,7 @@ $(document).ready(function() {
 
 <script>
     $(document).ready(function() {
-        const wcStep = 25;
+        const wcStep = 10;
         const wcStepLarge = 100;
         const imgStep = 1;
         const wcMin = 0;
