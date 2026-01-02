8f5041e460d6474588120f4497eab8abff4700d3
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2026-01-02 11:50:22 +0530

    243

--- resources/views/websites/gaming/twohundredfifty-three.blade.php
+++ resources/views/websites/gaming/twohundredfifty-three.blade.php
@@ -14,17 +14,17 @@
     }
 </style>
 
-<body style="padding: 0px; margin: 0px;">
+<body style="padding: 0px; margin: 0px;background-image: linear-gradient(to right, #020524 70%, #F43D87 70%);">
     <table width="100%" cellspacing="0" cellpadding="0" border="0">
         <tr style="width:100%;">
             <td align="center" style="padding: 0px;" style="width:100%;">
-                <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#0000000"
+                <table width="100%" cellspacing="0" cellpadding="0" border="0"
                     style="border-collapse: collapse; " >
 
                     <!-- Content -->
                     <tr style="width:100%;">
                         <td
-                            style="padding: 95px 120px 170px 40px;background: url({{ $invoice_image1 }});background-position: center;background-size: cover;height:444px;" style="width:100%;">
+                            style="padding: 95px 120px 170px 40px;background: url({{ $invoice_image1 }});background-position: center top;background-size: cover;height:444px;" style="width:100%;">
                             <table style="width:100%;">
                                 <tr>
                                     <td>
