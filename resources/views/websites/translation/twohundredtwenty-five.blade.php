9080925ad3a43e657f8d92d891df7aa813fb10f9
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2026-01-23 12:05:02 +0530

    225

--- resources/views/websites/translation/twohundredtwenty-five.blade.php
+++ resources/views/websites/translation/twohundredtwenty-five.blade.php
@@ -64,17 +64,17 @@
                                         <table cellpadding="5" cellspacing="0" style="width: 100%; font-size: 8px;">
                                             <tr>
                                                 <!-- <td style="font-weight: bold;">Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $company_name }}</td> -->
-                                                <td style="font-weight: bold;max-width: 250px;">Name</td>
+                                                <td style="font-weight: bold;min-width: 250px;max-width: 280px;">Name</td>
                                                 <td>{{ $company_name }}</td>
                                             </tr>
                                             <tr>
                                                 <!-- <td style="font-weight: bold; vertical-align: top;">Address&nbsp;&nbsp;&nbsp;&nbsp;{{ $company_address }}</td> -->
-                                                <td style="font-weight: bold;max-width: 250px;vertical-align: top;">Address</td>
+                                                <td style="font-weight: bold;min-width: 250px;max-width: 280px;vertical-align: top;">Address</td>
                                                 <td>{{ $company_address }}</td>
                                             </tr>
                                             <tr>
                                                 <!-- <td style="font-weight: bold;">Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $company_email }}</td> -->
-                                                <td style="font-weight: bold;max-width: 250px;">Email</td>
+                                                <td style="font-weight: bold;min-width: 250px;max-width: 280px;">Email</td>
                                                 <td>{{ $company_email }}</td>
                                             </tr>
                                         </table>
