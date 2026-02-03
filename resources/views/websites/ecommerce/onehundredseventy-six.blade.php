0176ebfa348f09f949c394d1d4fc67c210c8b237
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2026-02-03 12:16:29 +0530

    176

--- resources/views/websites/ecommerce/onehundredseventy-six.blade.php
+++ resources/views/websites/ecommerce/onehundredseventy-six.blade.php
@@ -143,11 +143,11 @@
                                 <td style="background: linear-gradient(to right, #e03b42 35%, #202020 35%); padding: 60px 40px 60px 40px; display: flex; justify-content: space-between;">
                             <p style="font-family: Arial;font-size: 22px;margin: 0px;font-weight: 400; color: #FFFFFF;">Thank You for<br>Purchasing!</p>
                             <p style="font-family: Arial;font-size: 9px;margin: 0px;font-weight: 400; color: #FFFFFF; text-align: end; line-height: 15px;"><span style="color: #e03b42; font-size: 14px;">Contact</span><br>
-                                {{ $company_name }}<br>
-                                {{ $company_email }}<br>
+                                {{!! $company_name !!}}<br>
                                 {!! $company_address !!}<br>
-                                {{ $company_mobile }}<br>
-                                {{ $site_name }}                                
+                                {{!! $company_email !!}}<br>
+                                {{!! $company_mobile !!}}<br>
+                                {{!! $site_name !!}}                                
                             </p>
                         </td>
     </tr>
