a05d7d050f4b09ff1d58dddcfcb878549fdeed44
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2026-02-20 14:20:53 +0530

    q

--- resources/views/websites/marketing/onehundredforty-one.blade.php
+++ resources/views/websites/marketing/onehundredforty-one.blade.php
@@ -13,7 +13,7 @@
 <body style="border-collapse: collapse; margin:0; padding:0; background:#E9E3D3; font-family:Arial, sans-serif;">
     <table width="100%" cellspacing="0" cellpadding="0" border="0" style="border-collapse: collapse;">
         <tr>
-            <td align="center" bgcolor="#FFF">
+            <td align="center" bgcolor="#E9E3D3">
                 <table  style="border-collapse: collapse;border: 0px;background-color: white;background-color: #E9E3D3; height 100vh;">
                     <!--Header-->
                     <tr style="background: url('{{ $invoice_header_image }}') no-repeat;background-position:center;background-size:contain; height: 145px;color: white;border-collapse: collapse;width: 600px;">
