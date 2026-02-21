59e0bb23f620e2f8864c1b50f61335afbe3a078b
Suyash Gurav <guravsuyash12@gmail.com>
2026-02-21 11:53:16 +0530

    resolved

--- resources/views/websites/marketing/twohundredfifty-five.blade.php
+++ resources/views/websites/marketing/twohundredfifty-five.blade.php
@@ -91,11 +91,7 @@
                                                     style="color:#000000; text-align:start;padding:8px;font-family:  Arial;font-size:18px;margin: 0px;font-weight: 400;">
                                                     {{ $customer_name }}
                                                 </p>
-                                                <!-- <p
-                                                    style="color:#000000; text-align:start;padding:0px;font-family:  Arial;font-size:10px;margin: 0px;font-weight: 400;">
-                                                    {{ $site_name }}<br>{{ $company_email }}
-                                                    | {{ $company_mobile }}
-                                                </p> -->
+                                               
                                                     {{ $site_name }}<br>{{ $customer_email ? $customer_email : '' }}
                                                     | {{ $customer_mobile ? $customer_mobile : '' }}
                                                 </p>
