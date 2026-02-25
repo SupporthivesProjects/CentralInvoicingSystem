acedf01abba881477bceab067ca7d4a0deee43d8
saakshichat <saakshi@supporthives.com>
2026-02-25 17:33:16 +0530

    adding date

--- resources/views/websites/ecommerce/onehundredforty-eight.blade.php
+++ resources/views/websites/ecommerce/onehundredforty-eight.blade.php
@@ -285,7 +285,8 @@
                                               <div class="invies-content">
                                                   <h2>INVOICE</h2>
                                                   <h6>Invoice No: #{{ $invoice_number }}</h6>
-                                                  <h6>Invoice Date: {{ $invoice_date }}</h6>
+                                                  <!-- <h6>Invoice Date: {{ $invoice_date }}</h6> -->
+                                                   <h6>Invoice Date: {{ \Carbon\Carbon::parse($user->created_at)->format('d/m/Y') }}</h6>
                                               </div>
                                           </td>
                                     </tr>
