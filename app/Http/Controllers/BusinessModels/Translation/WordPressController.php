66f9c88299dbfa9852a80c8125f26a9262071c74
Narayan Zade <narayan.d.zade@gmail.com>
2026-02-26 08:21:47 +0530

    added new more options to transalation site 36_48hrs

--- app/Http/Controllers/BusinessModels/Translation/WordPressController.php
+++ app/Http/Controllers/BusinessModels/Translation/WordPressController.php
@@ -928,13 +928,14 @@ class WordPressController extends Controller
                                 str_contains($productName, 'standard translation') ||
                                 str_contains($productName, 'business translation');
 
-                                
                     if ($isCertified) {
-                        $site->urgency_12h_per_page = $new_price;
-                        $site->urgency_24h_per_page = round($new_price / 2, 4);
+                        $site->urgency_12h_per_page    = $new_price;
+                        $site->urgency_24h_per_page    = round($new_price / 2, 4);
+                        $site->urgency_36_48h_per_page = round($new_price / 3, 4);
                     } elseif ($isStandard) {
-                        $site->urgency_12h_per_word = $new_price;
-                        $site->urgency_24h_per_word = round($new_price / 2, 4);
+                        $site->urgency_12h_per_word    = $new_price;
+                        $site->urgency_24h_per_word    = round($new_price / 2, 4);
+                        $site->urgency_36_48h_per_word = round($new_price / 3, 4);
                     }
 
                     $site->save();
