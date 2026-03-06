c77745a47cd091be2ae9da620edf2d5d11064549
Narayan Zade <narayan.d.zade@gmail.com>
2026-03-06 14:37:55 +0530

    wp giftcard fixing prices issue

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::middleware(['auth', 'role:admin,staff,developer'])->group(function () {
 
 
     Route::get('/game-api/{site_id}', [WebsiteController::class, 'gameSiteAPI'])->name('test.game.api');
-    Route::post('/giftcard-api/{site_id}', [WebsiteController::class, 'giftCardAPI'])->name('test.giftcard.api');
+    Route::get('/giftcard-api/{site_id}', [WebsiteController::class, 'giftCardAPI'])->name('test.giftcard.api');
 
     Route::get('/website/create', [WebsiteController::class, 'addwebsite'])->name('website.create');
     Route::post('/website', [WebsiteController::class, 'createWebsite'])->name('website.store');
