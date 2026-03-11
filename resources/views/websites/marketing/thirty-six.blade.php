9a2f318d24258f1b21a44bec1a834ee3971b3414
saakshichat <saakshi@supporthives.com>
2026-03-11 14:17:28 +0530

    new changes

--- resources/views/websites/marketing/thirty-six.blade.php
+++ resources/views/websites/marketing/thirty-six.blade.php
@@ -191,13 +191,13 @@
 
                                 <!-- Company Address -->
                                 <div style="flex: 1; text-align: left;">
-                                    <p style="font-size: 11px; margin: 0;font-family:Calibri;font-weight:700;"><b>ADDRESS</b></p>
+                                    <p style="font-size: 11px; margin: 0;font-family:Calibri;font-weight:700;"><b>ADDRESS</b></p><br>
                                     <p style="font-size: 9px; margin: 0;font-family:Calibri;">{!! $company_address ?? 'N/A' !!}</p>
                                 </div>
 
                                 <!-- Company Contact -->
                                 <div style="flex: 1; text-align: left;">
-                                    <p style="font-size: 11px; margin: 0;font-family:Calibri;font-weight:700;"><b>CONTACTS</b></p>
+                                    <p style="font-size: 11px; margin: 0;font-family:Calibri;font-weight:700;"><b>CONTACTS</b></p><br>
                                     <p style="font-size: 9px; margin: 0;font-family:Calibri;">
                                         {{ $company_email ?? 'support@bluemoonmarketeers.com' }}
                                     </p>
