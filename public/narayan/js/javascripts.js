cdcfa4717ca9fce7704adc867541f62f2dcb4df2
Narayan Zade <narayan.d.zade@gmail.com>
2026-03-12 20:22:25 +0530

    updating loader

--- public/narayan/js/javascripts.js
+++ public/narayan/js/javascripts.js
@@ -169,35 +169,16 @@ function replaceFeatherIconsTemporarily() {
     
 
 
-// function getLoaderRowHTML(colspan = 6) {
-//     return `
-//         <tr id="loaderRow">
-//             <td colspan="${colspan}" class="text-center py-4">
-//                 <svg class="pl" width="240" height="240" viewBox="0 0 240 240">
-//                     <circle class="pl__ring pl__ring--a" cx="120" cy="120" r="105" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 660" stroke-dashoffset="-330" stroke-linecap="round"></circle>
-//                     <circle class="pl__ring pl__ring--b" cx="120" cy="120" r="35" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 220" stroke-dashoffset="-110" stroke-linecap="round"></circle>
-//                     <circle class="pl__ring pl__ring--c" cx="85" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
-//                     <circle class="pl__ring pl__ring--d" cx="155" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
-//                 </svg>
-//             </td>
-//         </tr>
-//     `;
-// }
-
-
 function getLoaderRowHTML(colspan = 6) {
     return `
         <tr id="loaderRow">
-            <td colspan="${colspan}">
-                <div class="orbit-loader-wrapper">
-                    <div class="orbit-container">
-                        <div class="orbit-ring"></div>
-                        <div class="orbit-ring"></div>
-                        <div class="orbit-ring"></div>
-                        <div class="orbit-ring"></div>
-                        <div class="orbit-label">loading</div>
-                    </div>
-                </div>
+            <td colspan="${colspan}" class="text-center py-4">
+                <svg class="pl" width="240" height="240" viewBox="0 0 240 240">
+                    <circle class="pl__ring pl__ring--a" cx="120" cy="120" r="105" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 660" stroke-dashoffset="-330" stroke-linecap="round"></circle>
+                    <circle class="pl__ring pl__ring--b" cx="120" cy="120" r="35" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 220" stroke-dashoffset="-110" stroke-linecap="round"></circle>
+                    <circle class="pl__ring pl__ring--c" cx="85" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
+                    <circle class="pl__ring pl__ring--d" cx="155" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
+                </svg>
             </td>
         </tr>
     `;
