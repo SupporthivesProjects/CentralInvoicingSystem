454bbb8a7ef6bb3695de580d1b151c1171683d20
saakshichat <saakshi@supporthives.com>
2026-03-14 16:48:43 +0530

    adding font

--- resources/views/websites/marketing/thirty-six.blade.php
+++ resources/views/websites/marketing/thirty-six.blade.php
@@ -4,6 +4,19 @@
 <head>
     <title>{{ $site_name }} - Invoice #{{ $invoice_number }}</title>
     <style>
+        @font-face {
+    font-family: 'calibri';
+    src: url('../fonts/calibri-regular.ttf') format('truetype');
+    font-weight: 400;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'calibri';
+    src: url('../fonts/calibri-bold.ttf') format('truetype');
+    font-weight: 700;
+    font-style: normal;
+}
         * {
             margin:0px;
             padding:0px;
