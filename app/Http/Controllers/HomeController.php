5abfa70a9e16b0a287ebeee4fea041512ef98463
Narayan Zade <narayan.d.zade@gmail.com>
2026-03-16 10:58:54 +0530

    removed caching from home controler

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,16 +35,8 @@ class HomeController extends Controller
     
         $tempDownCount = Website::where('site_status', 'tdown')->count();
         $permDownCount = Website::where('site_status', 'pdown')->count();
-    
-        $currentHost = $request->getHost();
-        if ($currentHost === '127.0.0.1') {
-            $invoices = InvoiceGenerationHistory::orderBy('id', 'desc')->take(10)->get();
-        } else {
-            $invoices = Cache::remember('invoices.all', 300, function () {
-                return InvoiceGenerationHistory::orderBy('id', 'desc')->take(10)->get();
-            });
-        }
-    
+        $invoices = InvoiceGenerationHistory::orderBy('id', 'desc')->take(10)->get();
+        
         return view('pages.dashboard', compact('invoices', 'dates', 'invoiceCounts', 'businessmodels', 'sites', 'priceChanges', 'invoicedates', 'userInvoices', 'tempDownCount', 'permDownCount'));
     }
 
