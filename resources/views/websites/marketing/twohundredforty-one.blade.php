879894f6d8a2ce4d226898298d30c4c9d62e3ecd
Narayan Zade <narayan.d.zade@gmail.com>
2026-06-25 16:31:11 +0530

    push original devteam old

--- resources/views/websites/marketing/twohundredforty-one.blade.php
+++ resources/views/websites/marketing/twohundredforty-one.blade.php
@@ -89,7 +89,7 @@
                                     <td style="font-size:14px; line-height:18px; padding:10px 50px 5px 50px;">
                                         <img src="{{ $invoice_image3 }}" alt=""
                                             style="position: absolute ;   width: 30px;top: -47px;left: 10px;">
-                                        <strong style="color:#3C8F89; ">To:</strong><br>
+                                        <strong style="color:#3C8F89;font-weight:bold; ">To:</strong><br>
                                         {{ $customer_name ? $customer_name : '' }}<br>
                                         {{ $customer_email ? $customer_email : '' }}<br>
                                         {{ $customer_mobile ? $customer_mobile : '' }}
