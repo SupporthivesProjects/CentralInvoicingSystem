ffe0042e8dab332ab359a7e3741f91902fa8179b
Narayan <narayan.d.zade@gmail.com>
2026-07-29 10:49:50 +0530

    fixing issue of site no 77

--- resources/views/websites/contentwriting/seventy-seven.blade.php
+++ resources/views/websites/contentwriting/seventy-seven.blade.php
@@ -104,7 +104,7 @@
                                     </td>
                                 </tr>
                             </table>
-                            <div style="min-height:495px !important;">
+                            <div style="min-height:505px !important;">
                             <table width="100%" cellpadding="10" cellspacing="0"
                                 style="font-family: 'Montserrat'; border-collapse: collapse; font-size: 9px;">
                                 <!-- Header Row -->
