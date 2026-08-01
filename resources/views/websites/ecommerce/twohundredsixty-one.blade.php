30505e3a2b8174c86d072a30eee2338ab79ccdf5
saakshichat <saakshi@supporthives.com>
2026-08-01 16:03:32 +0530

    new one

--- resources/views/websites/ecommerce/twohundredsixty-one.blade.php
+++ resources/views/websites/ecommerce/twohundredsixty-one.blade.php
@@ -73,7 +73,7 @@
         </tr>
         <!-- Table Header -->
         <tr>
-            <td style="height: 585px;">
+            <td style="height:400px;">
                 <table cellpadding="0" cellspacing="0" style="margin: 0px 48px; width: fit-content;">
                     <thead>
                         <tr style="background:#263b6b;">
