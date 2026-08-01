0860b31f5b42aad0114205c09f9857a2f82b6944
saakshichat <saakshi@supporthives.com>
2026-08-01 15:36:06 +0530

    new

--- resources/views/websites/ecommerce/twohundredsixty-one.blade.php
+++ resources/views/websites/ecommerce/twohundredsixty-one.blade.php
@@ -10,7 +10,7 @@
             padding: 0px;
             background: #ececec;
             font-family: Arial, Helvetica, sans-serif;
-            height:100vh !important;
+            height:60vh !important;
         }
         table {
             border-collapse: collapse;
