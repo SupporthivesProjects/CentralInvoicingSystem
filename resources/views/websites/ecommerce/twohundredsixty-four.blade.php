208ea969ff4361619b1cb0e4ce3d8a831b427370
Rohit <rohit@supporthives.com>
2026-08-01 15:45:53 +0530

    new invoice

---
+++ resources/views/websites/ecommerce/twohundredsixty-four.blade.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Vizualuxe</title>
+</head>
+
+<body>
+    <table width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tr>
+            <td align="center" bgcolor="#f2f2f2" style="padding: 20px 0;">
+                <table width="600" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff"
+                    style="border-collapse: collapse; background: url(./img/bg.png); box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);">
+                    <!-- Header -->
+                    <tr>
+                        <td style="padding: 0; height: 90px;">
+                            <table width="100%" cellspacing="0" cellpadding="0" style="border-collapse: collapse;">
+                                <tr style="position: relative;">
+                                    <td style="
+                                        background: url('./img/header.PNG') no-repeat center;
+                                        background-size: cover;
+                                        height: 198px;">
+                                        <table style="margin-left: auto;
+                                             margin-right: auto; margin-bottom: 28px;">
+                                            <tr>
+                                                <td>
+                                                    <img src="./img/logo.png" alt="" style="width: 230px; " >
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <!-- Header End -->
+
+
+                    <!-- Content -->
+                    <tr>
+                        <td
+                            style="padding-top:0px;background: url(./img/bg.png); background-repeat: no-repeat;background-position: center;background-size: cover; font-family: 'Calibri';">
+                            <table align="center" cellpadding="0" cellspacing="0" border="0" width="900" style="padding: 90px 80px 0px 80px;">
+
+                                <!-- Header -->
+                                <tr>
+                                    <td>
+                                        <table width="100%" cellpadding="10" cellspacing="0" border="0"
+                                            style="background:#385b4b;color:#ffffff;">
+                                            <tr>
+                                                <td style="font-size:14px;font-weight:bold;text-transform:uppercase;">
+                                                    INVOICE NO. 1002
+                                                </td>
+
+                                                <td align="right"
+                                                    style="font-size:14px;font-weight:bold;text-transform:uppercase;">
+                                                    DATE 11/10/22
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+
+                                <!-- Space -->
+                                <tr>
+                                    <td height="30"></td>
+                                </tr>
+
+                                <!-- Bill To -->
+                                <tr>
+                                    <td style="" >
+
+                                        <table width="100%" cellpadding="0" cellspacing="0">
+
+                                            <tr>
+
+                                                <td width="50%" align="center">
+
+                                                    <div style="font-size:10px;font-weight:bold;color:#385b4b;">
+                                                        BILL TO
+                                                    </div>
+                                                    <div style="border-top:1px solid #385b4b;"></div>
+                                                    <div style="margin-top:6px;font-size:10px;color:#385b4b;">
+                                                        Customer Name Here
+                                                    </div>
+
+                                                </td>
+
+                                                <td width="50%" align="center" >
+
+                                                    <div style="font-size:10px;font-weight:bold;color:#385b4b;">
+                                                        BILLED FROM
+                                                    </div>
+                                                    <div style="border-top:1px solid #385b4b;"></div>
+                                                    <div style="margin-top:6px;font-size:10px;color:#385b4b; ">
+                                                        Vizualuxe.Com
+                                                    </div>
+
+                                                </td>
+
+                                            </tr>
+
+                                        </table>
+
+                                    </td>
+                                </tr>
+
+                                <!-- Space -->
+                                <tr>
+                                    <td height="25"></td>
+                                </tr>
+
+                                <!-- Invoice Table -->
+                                <tr>
+                                    <td>
+
+                                        <table width="100%" cellpadding="8" cellspacing="0" border="0" style="font-size: 10px;">
+
+                                            <tr style="background:#385b4b;color:#ffffff;font-weight:bold; font-size: 10px;">
+
+                                                <td width="15%">Quantity</td>
+                                                <td width="50%">Description</td>
+                                                <td width="17%" align="right">Unit Price </td>
+                                                <td width="18%" align="right">Total</td>
+
+                                            </tr>
+
+                                            <tr style="font-family: 'Cambria';">
+                                                <td style="border-bottom:1px solid #d9d9d9;color:#385b4b;">1</td>
+                                                <td style="border-bottom:1px solid #d9d9d9;color:#385b4b;">Blogging
+                                                    Traffic System</td>
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #d9d9d9;color:#385b4b;">12.00</td>
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #d9d9d9;color:#385b4b;">12.00</td>
+                                            </tr>
+
+                                            <tr style="font-family: 'Cambria'; background-color: #F2F2F2;">
+                                                <td style="border-bottom:1px solid #d9d9d9;color:#385b4b;">1</td>
+                                                <td style="border-bottom:1px solid #d9d9d9;color:#385b4b;">Blogging
+                                                    Traffic System</td>
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #d9d9d9;color:#385b4b;">12.00</td>
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #d9d9d9;color:#385b4b;">12.00</td>
+                                            </tr>
+
+                                            <tr style="font-family: 'Cambria';">
+                                                <td style="border-bottom:1px solid #d9d9d9;color:#385b4b;">1</td>
+                                                <td style="border-bottom:1px solid #d9d9d9;color:#385b4b;">Blogging
+                                                    Traffic System</td>
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #d9d9d9;color:#385b4b;">12.00</td>
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #d9d9d9;color:#385b4b;">12.00</td>
+                                            </tr>
+
+                                            <tr style="font-family: 'Cambria'; background-color: #F2F2F2;">
+                                                <td style="border-bottom:1px solid #d9d9d9;color:#385b4b;">1</td>
+                                                <td style="border-bottom:1px solid #d9d9d9;color:#385b4b;">Blogging
+                                                    Traffic System</td>
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #d9d9d9;color:#385b4b;">12.00</td>
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #d9d9d9;color:#385b4b;">12.00</td>
+                                            </tr>
+
+                                            <!-- Empty Rows -->
+
+                                            <tr>
+                                                <td colspan="4" style="height:13px;border-bottom:1px solid #d9d9d9;">
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="4" style="height:13px;border-bottom:1px solid #d9d9d9; background-color: #F2F2F2;">
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="4" style="height:13px;border-bottom:1px solid #d9d9d9;">
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="4" style="height:13px;border-bottom:1px solid #d9d9d9; background-color: #F2F2F2;">
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="4" style="height:13px;border-bottom:1px solid #d9d9d9;">
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="4" style="height:13px;border-bottom:1px solid #d9d9d9; background-color: #F2F2F2;">
+                                                </td>
+                                            </tr>
+
+                                        </table>
+
+                                    </td>
+                                </tr>
+
+                                <!-- Space -->
+                                <tr>
+                                    <td height="15"></td>
+                                </tr>
+
+                                <!-- Totals -->
+                                <tr>
+                                    <td align="right">
+
+                                        <table width="400" cellpadding="8" cellspacing="0" border="0">
+
+                                            <tr>
+                                                <td style="border-bottom:1px solid #cccccc;color:#385b4b; font-size: 10px;">
+                                                    SUBTOTAL
+                                                </td>
+
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #cccccc;color:#385b4b; font-size: 10px;">
+                                                    900.00
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+                                                <td style="border-bottom:1px solid #cccccc;color:#385b4b; font-size: 10px; background-color: #F2F2F2;">
+                                                    DISCOUNT APPLIED
+                                                </td>
+
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #cccccc;color:#385b4b; font-size: 10px; background-color: #F2F2F2;">
+                                                    24.99
+                                                </td>
+                                            </tr>
+
+                                            <tr>
+
+                                                <td
+                                                    style="border-bottom:1px solid #cccccc;font-size:14px;font-weight:bold;color:#385b4b;">
+                                                    TOTAL
+                                                </td>
+
+                                                <td align="right"
+                                                    style="border-bottom:1px solid #cccccc;font-size:14px;font-weight:bold;color:#385b4b;">
+                                                    969.99
+                                                </td>
+
+                                            </tr>
+
+                                        </table>
+
+                                    </td>
+                                </tr>
+
+                            </table>
+                        </td>
+                    </tr>
+                    <!-- Content End-->
+
+
+                    <!-----------Footer----------->
+                    <tr>
+                        <td>
+                            <table width="100%" cellspacing="0" cellpadding="" border="0px"
+                                style="border-collapse: collapse; background-image: url(./img/footer.png); background-position: center; background-size: contain; background-repeat: no-repeat; height: 189px;">
+                                <tr style="font-size: 10px; font-family: 'Cambria';" >
+                                     <td  style="padding-right:20px; padding-left: 123px;">
+                                         <table cellpadding="0" cellspacing="0" border="0" style="margin-top: 40px;">
+                                             <tr>
+                                                 <td valign="mmiddle" style="padding-right:10px;">
+                                                     <img src="./img/logo2.png" alt="">
+                                                 </td>
+
+                                                 <td valign="middle">
+                                                     <span style="color:#ffffff;font-size:10px;font-weight:bold;">
+                                                         TBC
+                                                     </span>
+                                                 </td>
+                                             </tr>
+                                         </table>
+                                     </td>
+
+                                     <!-- Email -->
+                                     <td   style="padding-right:20px;">
+                                         <table cellpadding="0" cellspacing="0" border="0" style="margin-top: 40px;">
+                                             <tr>
+                                                 <td valign="middle" style="padding-right: 10px;">
+                                                     <img src="./img/contact.png" alt="">
+                                                 </td>
+
+                                                 <td valign="middle">
+                                                     <span style="color:#ffffff;font-size:10px;font-weight:bold;">
+                                                         Support@vizualuxe.com
+                                                     </span>
+                                                 </td>
+                                             </tr>
+                                         </table>
+                                     </td>
+
+                                     <!-- Address -->
+                                     <td >
+                                         <table cellpadding="0" style="margin-top: 80px;" cellspacing="0" border="0">
+                                             <tr>
+                                                 <td valign="top" style="padding-right:10px;">
+                                                    <img src="./img/location.png" alt="" style="width: 38px;">
+                                                 </td>
+
+                                                 <td style="color:#ffffff;font-size:10px;line-height:18px;">
+                                                     Aurelius Ventures L.L.C-FZ<br>
+                                                     Meydan Grandstand, 6th floor,<br>
+                                                     Meydan Road, Nad Al Sheba,<br>
+                                                     Dubai, U.A.E.
+                                                 </td>
+                                             </tr>
+                                         </table>
+                                     </td>
+                                </tr>
+
+                            </table>
+                        </td>
+                    </tr>
+                    <!-----------Footer End----------->
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
