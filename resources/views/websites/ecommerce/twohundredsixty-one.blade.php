98573c350c3b131b0b43238083d7e67b8acd5d87
saakshichat <saakshi@supporthives.com>
2026-08-01 15:41:06 +0530

    changes

--- resources/views/websites/ecommerce/twohundredsixty-one.blade.php
+++ resources/views/websites/ecommerce/twohundredsixty-one.blade.php
@@ -23,7 +23,7 @@
     </style>
 </head>
 <body>
-    <table class="invoice" >
+    <table class="invoice" style="max-height:100vh;">
         <!-- Logo -->
         <tr>
             <td align="center" style="padding: 36px 0px 28px 0px;">
